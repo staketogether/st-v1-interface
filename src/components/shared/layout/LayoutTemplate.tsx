e97ec6577b904928ba20867a3fd6844d76e0a42c
Fabricio Miranda <fabriciomirandabr@gmail.com>
2023-05-17 23:58:04 -0300

    feat: update layout

--- src/components/shared/layout/LayoutTemplate.tsx
+++ src/components/shared/layout/LayoutTemplate.tsx
@@ -28,6 +28,8 @@ const { Container, Wrapper, Content, Body } = {
     grid-template-rows: 1fr 60px;
     gap: 24px;
     min-height: 100vh;
+    /* Todo! Temp, remove later */
+    min-width: ${({ theme }) => theme.breakpoints.lg};
   `,
   Wrapper: styled.div`
     width: 100%;
@@ -35,17 +37,23 @@ const { Container, Wrapper, Content, Body } = {
     grid-template-columns: minmax(320px, ${({ theme }) => theme.breakpoints.xl});
     justify-content: center;
     place-items: start center;
+    /* Todo! Temp, remove later */
+    min-width: ${({ theme }) => theme.breakpoints.lg};
   `,
   Content: styled.div`
     display: grid;
     grid-template-columns: minmax(320px, ${({ theme }) => theme.breakpoints.xl});
     padding: ${props => props.theme.size[24]};
     gap: 48px;
+    /* Todo! Temp, remove later */
+    min-width: ${({ theme }) => theme.breakpoints.lg};
   `,
   Body: styled.div`
     display: grid;
     grid-template-columns: minmax(320px, ${({ theme }) => theme.breakpoints.xl});
     justify-content: center;
     place-items: center;
+    /* Todo! Temp, remove later */
+    min-width: ${({ theme }) => theme.breakpoints.lg};
   `
 }
