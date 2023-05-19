f184b22439aaa28654833f8b6100b5e6d01ad6f0
Fabricio Miranda <fabriciomirandabr@gmail.com>
2023-05-19 17:23:32 -0300

    fix: rollback ens

--- src/hooks/useEns.tsx
+++ src/hooks/useEns.tsx
@@ -1,21 +1,32 @@
 import { useEffect, useState } from 'react'
-import getEns from '../services/getEns'
+import { useEnsAvatar, useEnsName } from 'wagmi'
+import chainConfig from '../config/chain'
 
 export default function useEns(address: `0x${string}`) {
+  const { chainId } = chainConfig()
+
   const [name, setName] = useState<string | undefined>(undefined)
   const [avatar, setAvatar] = useState<string | undefined>(undefined)
 
+  const ensAvatar = useEnsAvatar({
+    address,
+    chainId,
+    cacheTime: 3000
+  })
+
+  const ensName = useEnsName({
+    address,
+    chainId,
+    cacheTime: 3000
+  })
+
   useEffect(() => {
-    const getEnsData = async () => {
-      const ens = await getEns(address)
+    setAvatar(ensAvatar.data?.toString())
+  }, [ensAvatar.data])
 
-      setAvatar(ens.avatar)
-
-      setName(ens.name)
-    }
-
-    getEnsData()
-  }, [address])
+  useEffect(() => {
+    setName(ensName.data?.toString())
+  }, [ensName.data])
 
   return { address, name, avatar }
 }
