5a07e88af139581f55241483f000be6f1b599264
Fabricio Miranda <fabriciomirandabr@gmail.com>
2023-06-01 18:48:16 -0300

    fix: extra data

--- src/queries/queryCommunity.ts
+++ src/queries/queryCommunity.ts
@@ -3,26 +3,10 @@ import { gql } from '@apollo/client'
 export const queryCommunity = gql`
   query Community($id: String) {
     community(id: $id) {
-      id
       address
       delegatedShares
       rewardsShares
       receivedDelegationsCount
-      delegations {
-        id
-        delegationShares
-        delegate {
-          address
-          shares
-          sentDelegationsCount
-          rewardsShares
-        }
-        delegated {
-          address
-          delegatedShares
-          receivedDelegationsCount
-        }
-      }
     }
   }
 `
