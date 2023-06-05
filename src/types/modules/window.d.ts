2783e8e096fb7894eb8859859d496a9877dd4d57
Fabricio Miranda <fabriciomirandabr@gmail.com>
2023-06-05 20:53:40 -0300

    fix: type

--- src/types/modules/window.d.ts
+++ src/types/modules/window.d.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/prefer-default-export
 export declare global {
   interface Window {
-    gtag: (...args: any[]) => void
+    gtag: (...args: unknown[]) => void
     dataLayer: { event: string; page: string }[]
   }
 }
