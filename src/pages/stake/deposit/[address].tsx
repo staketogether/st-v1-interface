715af2bb52755124363fa0d73bc507eb4b610ebd
Fabricio Miranda <fabriciomirandabr@gmail.com>
2023-06-05 18:13:23 -0300

    feat: add metadata for pool

--- src/pages/stake/deposit/[address].tsx
+++ src/pages/stake/deposit/[address].tsx
@@ -2,21 +2,26 @@ import { GetServerSideProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import LayoutHead from '../../../components/shared/layout/LayoutHead'
 import LayoutTemplate from '../../../components/shared/layout/LayoutTemplate'
+import { MetaPool } from '../../../components/shared/meta/MetaPool'
 import StakeControl from '../../../components/stake/StakeControl'
 import usePool from '../../../hooks/subgraphs/usePool'
 import useTranslation from '../../../hooks/useTranslation'
+import { getEns } from '../../../services/getEns'
 
 type DepositPoolProps = {
   poolAddress: string
+  name?: string
+  avatar?: string
 }
 
-export default function Deposit({ poolAddress }: DepositPoolProps) {
+export default function Deposit({ poolAddress, name, avatar }: DepositPoolProps) {
   const { t } = useTranslation()
 
   const { pool } = usePool(poolAddress)
 
   return (
     <LayoutTemplate>
+      <MetaPool name={name} avatar={avatar} />
       <LayoutHead text={t('titles.stake')} />
       <StakeControl pool={pool} type='deposit' />
     </LayoutTemplate>
@@ -26,10 +31,17 @@ export default function Deposit({ poolAddress }: DepositPoolProps) {
 export const getServerSideProps: GetServerSideProps = async context => {
   const params = context?.params as { address: `0x${string}` } | undefined
 
-  // const { data } = await apolloClient.query<{ pool: Pool } | undefined>({
-  //   query: queryPool,
-  //   variables: { id: params?.address || '' }
-  // })
+  if (params?.address) {
+    const ens = await getEns(params?.address)
+    return {
+      props: {
+        ...(await serverSideTranslations(context.locale || 'en', ['common'], null, ['en'])),
+        poolAddress: params?.address || '',
+        name: ens.name,
+        avatar: ens.avatar
+      }
+    }
+  }
 
   return {
     props: {
