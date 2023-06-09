4a3d6b648c3de9f1e69d88f583e18251aa3dc347
Paulo Barcelos <paulo.barcelosti@gmail.com>
2023-06-09 16:17:34 -0300

    fix: fix prettier

--- src/hooks/analytics/useMixpanelAnalytics.tsx
+++ src/hooks/analytics/useMixpanelAnalytics.tsx
@@ -1,5 +1,5 @@
 import mixpanel from 'mixpanel-browser'
-import { useCallback, useState } from "react";
+import { useCallback, useState } from 'react'
 
 export const useMixpanelAnalytics = () => {
   const [isInitialized, setIsInitialized] = useState(false)
@@ -16,58 +16,70 @@ export const useMixpanelAnalytics = () => {
     setIsInitialized(true)
   }, [])
 
-  const registerConnectWallet = useCallback((account: string, chainId: number) => {
-    if (!isInitialized) {
-      return
-    }
-    
-    mixpanel.track('Wallet Connected', {
-      walletAddress: account,
-      chainId: chainId,
-      path: window.location.pathname
-    })
-  }, [isInitialized])
+  const registerConnectWallet = useCallback(
+    (account: string, chainId: number) => {
+      if (!isInitialized) {
+        return
+      }
 
-  const registerPageView = useCallback((account: string, chainId: number) => {
-    if (!isInitialized) {
-      return
-    }
+      mixpanel.track('Wallet Connected', {
+        walletAddress: account,
+        chainId: chainId,
+        path: window.location.pathname
+      })
+    },
+    [isInitialized]
+  )
 
-    mixpanel.track('Page View', {
-      path: window.location.pathname,
-      referrer: window.document.referrer,
-      walletAddress: account,
-      chainId: chainId
-    })
-  }, [isInitialized])
+  const registerPageView = useCallback(
+    (account: string, chainId: number) => {
+      if (!isInitialized) {
+        return
+      }
 
-  const registerDeposit = useCallback((walletAddress: string, chainId: number, poolAddress: string, amount: string) => {
-    if (!isInitialized) {
-      return
-    }
+      mixpanel.track('Page View', {
+        path: window.location.pathname,
+        referrer: window.document.referrer,
+        walletAddress: account,
+        chainId: chainId
+      })
+    },
+    [isInitialized]
+  )
 
-    mixpanel.track('Deposit', {
-      walletAddress,
-      chainId,
-      amount,
-      poolAddress,
-      path: window.location.pathname
-    })
-  }, [isInitialized])
+  const registerDeposit = useCallback(
+    (walletAddress: string, chainId: number, poolAddress: string, amount: string) => {
+      if (!isInitialized) {
+        return
+      }
 
-  const registerWithdraw = useCallback((walletAddress: string, chainId: number, poolAddress: string, amount: string) => {
-    if (!isInitialized) {
-      return
-    }
+      mixpanel.track('Deposit', {
+        walletAddress,
+        chainId,
+        amount,
+        poolAddress,
+        path: window.location.pathname
+      })
+    },
+    [isInitialized]
+  )
 
-    mixpanel.track('Withdraw', {
-      walletAddress,
-      chainId,
-      amount,
-      poolAddress,
-      path: window.location.pathname
-    })
-  }, [isInitialized])
+  const registerWithdraw = useCallback(
+    (walletAddress: string, chainId: number, poolAddress: string, amount: string) => {
+      if (!isInitialized) {
+        return
+      }
+
+      mixpanel.track('Withdraw', {
+        walletAddress,
+        chainId,
+        amount,
+        poolAddress,
+        path: window.location.pathname
+      })
+    },
+    [isInitialized]
+  )
 
   return {
     isInitialized,
