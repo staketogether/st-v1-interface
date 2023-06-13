918d2b5027c07d4b35ff86bd992f369e02f1bf19
Arthur Ribeiro <arthurmr13@hotmail.com>
2023-06-13 15:33:43 -0300

    fix: add missing validation

--- src/components/stake/StakeForm.tsx
+++ src/components/stake/StakeForm.tsx
@@ -99,7 +99,7 @@ export function StakeForm({ type, accountAddress, poolAddress }: StakeFormProps)
     if (isWrongNetwork) {
       return `${t('switch')} ${chain.name.charAt(0).toUpperCase() + chain.name.slice(1)}`
     }
-    if (insufficientFunds || insufficientWithdrawalLiquidity) {
+    if (insufficientFunds || insufficientWithdrawalLiquidity || insufficientMinDeposit) {
       return errorLabel
     }
     return actionLabel
