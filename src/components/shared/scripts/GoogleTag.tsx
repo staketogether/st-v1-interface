aa32e6f3bf0585de1071d2da3506bb00b1b0edd9
Fabricio Miranda <fabriciomirandabr@gmail.com>
2023-06-13 12:30:07 -0300

    fix: analytics

--- src/components/shared/scripts/GoogleTag.tsx
+++ src/components/shared/scripts/GoogleTag.tsx
@@ -5,15 +5,16 @@ export const GoogleTag = () => {
     <>
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-        strategy='afterInteractive'
+        strategy='lazyOnload'
       />
-      <Script id='google-analytics' strategy='afterInteractive'>
+      <Script id='google-analytics' strategy='lazyOnload'>
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-        
-          gtag('config', ${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID});
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
+            page_path: window.location.pathname,
+          });
         `}
       </Script>
     </>
