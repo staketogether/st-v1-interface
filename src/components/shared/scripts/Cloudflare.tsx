f36e9200bc33d10c27b5a926b8f122bb0116bd74
Fabricio Miranda <fabriciomirandabr@gmail.com>
2023-06-14 15:55:26 -0300

    feat: add cloudflare analytics

---
+++ src/components/shared/scripts/Cloudflare.tsx
@@ -0,0 +1,13 @@
+import Script from 'next/script'
+
+export const Cloudflare = () => {
+  return (
+    <Script
+      id='cloudflare-analytics'
+      defer
+      strategy='afterInteractive'
+      src='https://static.cloudflareinsights.com/beacon.min.js'
+      data-cf-beacon={`{"token": ${process.env.NEXT_PUBLIC_CLOUDFLARE_TOKEN}}'`}
+    />
+  )
+}
