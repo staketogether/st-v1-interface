73b912e5d29774c69e77d2a0b872c8e830ef4a92
Paulo Barcelos <46430156+paulobarcelosbr@users.noreply.github.com>
2023-06-16 14:17:18 -0300

    fix: fix loading icon (#33)

--- src/hooks/contracts/usePooledEthByShares.tsx
+++ src/hooks/contracts/usePooledEthByShares.tsx
@@ -1,21 +1,14 @@
 import { BigNumber } from 'ethers'
-import { useEffect, useState } from 'react'
 import chainConfig from '../../config/chain'
 import { useStakeTogetherPooledEthByShares } from '../../types/Contracts'
 
 export default function usePooledEthByShares(sharesAmount: BigNumber) {
   const { contracts } = chainConfig()
 
-  const [balance, setBalance] = useState<string>('0')
-
   const pooledEthBySharesReq = useStakeTogetherPooledEthByShares({
     address: contracts.StakeTogether,
     args: [sharesAmount]
   })
 
-  useEffect(() => {
-    setBalance(pooledEthBySharesReq.data?.toString() || '0')
-  }, [pooledEthBySharesReq.data])
-
-  return balance
+  return pooledEthBySharesReq.data?.toString() || '0'
 }
