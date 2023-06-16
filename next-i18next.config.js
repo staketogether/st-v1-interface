7e1b134a7ba18ea34d12571898db33831070dab2
Fabricio Miranda <fabriciomirandabr@gmail.com>
2023-06-15 21:50:26 -0300

    feat: add browser lang detection

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,9 @@ const path = require('path')
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en','pt']
+    locales: ['en', 'pt'],
+    localeDetection: true
   },
+  trailingSlash: true,
   localePath: path.resolve('./src/locales')
 }
