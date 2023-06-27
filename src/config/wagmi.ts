becac6651fac32b45e61e6ad725651f47980089c
Fabricio Miranda <fabriciomirandabr@gmail.com>
2023-06-27 18:02:58 -0300

    fix: only 1 chain

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -1,13 +1,30 @@
 import { goerli, localhost } from 'viem/chains'
 import { configureChains, createConfig, mainnet } from 'wagmi'
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
-import { publicProvider } from 'wagmi/providers/public'
+import chainConfig from './chain'
 
-const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [mainnet, goerli, localhost],
-  [publicProvider()],
-  { rank: true, retryCount: 3 }
-)
+const selectChainConfig = () => {
+  const { chainId } = chainConfig()
+
+  if (chainId === 1) {
+    return mainnet
+  }
+
+  if (chainId === 5) {
+    return goerli
+  }
+
+  if (chainId === 31337) {
+    return localhost
+  }
+
+  throw new Error('Chain not supported')
+}
+
+const { chains, publicClient, webSocketPublicClient } = configureChains([selectChainConfig()], [], {
+  rank: true,
+  retryCount: 3
+})
 
 const config = createConfig({
   autoConnect: true,
