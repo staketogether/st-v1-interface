c34d6fd4be0dc09c12bca6bffdd934fd30172e73
Arthur Ribeiro <arthurmr13@hotmail.com>
2023-07-03 12:26:11 -0300

    chore: obtain firebase credentials in a way vercel can read

--- firebase.ts
+++ firebase.ts
@@ -1,7 +1,27 @@
 import admin from 'firebase-admin';
 
-if (!admin.instanceId) {
-  admin.initializeApp();
+if (!process.env.NEXT_PUBLIC_FIREBASE_CREDENTIALS_CLIENT_EMAIL) {
+    throw new Error('Missing env variable NEXT_PUBLIC_FIREBASE_CREDENTIALS_CLIENT_EMAIL');
 }
+
+if (!process.env.NEXT_PUBLIC_FIREBASE_CREDENTIALS_PRIVATE_KEY) {
+    throw new Error('Missing env variable NEXT_PUBLIC_FIREBASE_CREDENTIALS_PRIVATE_KEY');
+}
+
+if (!process.env.NEXT_PUBLIC_FIREBASE_CREDENTIALS_PROJECT_ID) {
+    throw new Error('Missing env variable NEXT_PUBLIC_FIREBASE_CREDENTIALS_PROJECT_ID');
+}
+
+const clientEmail = process.env.NEXT_PUBLIC_FIREBASE_CREDENTIALS_CLIENT_EMAIL;
+const privateKey = process.env.NEXT_PUBLIC_FIREBASE_CREDENTIALS_PRIVATE_KEY.replace(/\\n/g, '\n');
+const projectId = process.env.NEXT_PUBLIC_FIREBASE_CREDENTIALS_PROJECT_ID;
+
+admin.initializeApp({
+    credential: admin.credential.cert({
+        clientEmail,
+        privateKey,
+        projectId,
+    })
+  });
 
 export const firestore = admin.firestore();
