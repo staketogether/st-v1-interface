c18c831faf1172dc84c1fdb9da418bf771ba2f3f
Arthur Ribeiro <arthurmr13@hotmail.com>
2023-07-03 15:37:02 -0300

    chore: fix prettier

--- src/types/CreatorPasscode.ts
+++ src/types/CreatorPasscode.ts
@@ -1,29 +1,29 @@
-import { DocumentData, QueryDocumentSnapshot } from 'firebase-admin/firestore';
+import { DocumentData, QueryDocumentSnapshot } from 'firebase-admin/firestore'
 
 export interface CreatorPasscode {
-    passcode: string;
-    creator: string;
-    chainId: number;
-    ethLimit: string;
-    amountToSend: string;
-    accountsDistributed: string[];
-    ipsUsed: string[];
+  passcode: string
+  creator: string
+  chainId: number
+  ethLimit: string
+  amountToSend: string
+  accountsDistributed: string[]
+  ipsUsed: string[]
 }
 
 export const creatorPasscodeConverter = {
-    toFirestore(creatorPasscode: CreatorPasscode): DocumentData {
-        return creatorPasscode;
-    },
-    fromFirestore(snapshot: QueryDocumentSnapshot): CreatorPasscode {
-        const data = snapshot.data();
-        return {
-            passcode: data.passcode,
-            creator: data.creator,
-            chainId: data.chainId,
-            ethLimit: data.ethLimit,
-            amountToSend: data.amountToSend,
-            ipsUsed: data.ipsUsed,
-            accountsDistributed: data.accountsDistributed
-        };
+  toFirestore(creatorPasscode: CreatorPasscode): DocumentData {
+    return creatorPasscode
+  },
+  fromFirestore(snapshot: QueryDocumentSnapshot): CreatorPasscode {
+    const data = snapshot.data()
+    return {
+      passcode: data.passcode,
+      creator: data.creator,
+      chainId: data.chainId,
+      ethLimit: data.ethLimit,
+      amountToSend: data.amountToSend,
+      ipsUsed: data.ipsUsed,
+      accountsDistributed: data.accountsDistributed
     }
+  }
 }
