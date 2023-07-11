012976474bf2c64ef7540f59dfa7d1d48da14dd5
Paulo Barcelos <paulo.barcelosti@gmail.com>
2023-07-11 12:13:18 -0300

    feat: add new header on pages

--- src/components/shared/layout/LayoutHead.tsx
+++ src/components/shared/layout/LayoutHead.tsx
@@ -19,7 +19,9 @@ const { Head, Title } = {
   `,
   Title: styled.h1`
     font-size: ${({ theme }) => theme.font.size[32]};
-    font-weight: 400;
     color: ${({ theme }) => theme.color.primary};
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
   `
 }
