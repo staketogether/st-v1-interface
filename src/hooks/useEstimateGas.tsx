b2629f46449696f7144a8b3d2fdf4f32494e84f8
Paulo Barcelos <paulo.barcelosti@gmail.com>
2023-07-23 21:59:41 -0300

    fix: add try in useEstimateGas

--- src/hooks/useEstimateGas.tsx
+++ src/hooks/useEstimateGas.tsx
@@ -7,9 +7,13 @@ export default function useEstimateGas(tx?: ethers.TransactionRequest) {
   const { provider } = chainConfig()
   useEffect(() => {
     const estimateGas = async () => {
-      const gasEstimate = await provider.estimateGas(tx as ethers.TransactionRequest)
-      const gasLimit = gasEstimate + gasEstimate / BigInt(5)
-      setEstimateGas(gasLimit)
+      try {
+        const gasEstimate = await provider.estimateGas(tx as ethers.TransactionRequest)
+        const gasLimit = gasEstimate + gasEstimate / BigInt(5)
+        setEstimateGas(gasLimit)
+      } catch (error) {
+        console.error('Error estimate Gas:', error)
+      }
     }
 
     if (tx) {
