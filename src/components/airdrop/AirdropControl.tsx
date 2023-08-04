9659feb876dc1cdfdc0d4351fa5f5ada810f079d
Paulo Barcelos <paulo.barcelosti@gmail.com>
2023-08-04 20:40:37 -0300

    feat: finish page

--- src/components/airdrop/AirdropControl.tsx
+++ src/components/airdrop/AirdropControl.tsx
@@ -5,54 +5,81 @@ import React from 'react'
 import { AiFillCheckCircle, AiOutlineClose, AiOutlineQuestionCircle } from 'react-icons/ai'
 import { FiExternalLink } from 'react-icons/fi'
 import styled from 'styled-components'
+import WalletSidebarDisconnected from '../shared/wallet/WalletSidebarDisconnected'
+import useWalletSidebarConnectWallet from '@/hooks/useWalletSidebarConnectWallet'
+import stIcon from '@assets/icons/seth-icon.svg'
+import Image from 'next/image'
+import EnsName from '../shared/ens/EnsName'
 
 export default function AirdropControl() {
-  const { accountIsConnected } = useConnectedAccount()
+  const { accountIsConnected, account } = useConnectedAccount()
   const { t } = useTranslation()
+  const { setOpenSidebarConnectWallet } = useWalletSidebarConnectWallet()
   return (
-    <Container>
-      <AirdropContainer>
-        <h1>{t('airdrop.title')}</h1>
-        <p>{t('airdrop.description')}</p>
-        {!accountIsConnected && (
-          <WalletDisconnectedContainer>
-            <button>{t('connectWalletSideBar.connectButton')}</button>
-            <button className='ghost'>{t('airdrop.readAnnouncementButton')}</button>
-          </WalletDisconnectedContainer>
-        )}
-        <DescribeContainer>
-          <header>
-            <h3>{t('airdrop.eligibilityCriteria')}</h3>
-            <div>
-              <span>{t('learnMore')}</span>
-              <ExternalLink />
-            </div>
-          </header>
-        </DescribeContainer>
-        <EligibilityList>
-          <Row>
-            <div>
-              <VerifiedIcon />
-              {t('airdrop.pool')}
-              <Tooltip title='Pools'>
-                <QuestionIcon />
-              </Tooltip>
-            </div>
-            <span>{`0 ${t('lsd.symbol')}`}</span>
-          </Row>
-          <Row>
-            <div>
-              <ExcludeIcon />
-              {t('airdrop.locks')}
-              <Tooltip title='Pools'>
-                <QuestionIcon />
-              </Tooltip>
-            </div>
-            <span>{`0 ${t('lsd.symbol')}`}</span>
-          </Row>
-        </EligibilityList>
-      </AirdropContainer>
-    </Container>
+    <>
+      <Container>
+        <AirdropContainer>
+          <h1>{t('airdrop.title')}</h1>
+          <p>{t('airdrop.description')}</p>
+          {accountIsConnected && account && (
+            <WalletConnectedContainer>
+              <div>
+                <EnsName large address={account} />
+                <div>
+                  <span>0</span>
+                  <Image src={stIcon} width={24} height={24} alt='staked Icon' />
+                </div>
+              </div>
+              <button>{t('airdrop.startClaimProcess')}</button>
+            </WalletConnectedContainer>
+          )}
+          {!accountIsConnected && (
+            <WalletDisconnectedContainer>
+              <button
+                onClick={() => {
+                  setOpenSidebarConnectWallet(true)
+                }}
+              >
+                {t('connectWalletSideBar.connectButton')}
+              </button>
+              <button className='ghost'>{t('airdrop.readAnnouncementButton')}</button>
+            </WalletDisconnectedContainer>
+          )}
+          <DescribeContainer>
+            <header>
+              <h3>{t('airdrop.eligibilityCriteria')}</h3>
+              <div>
+                <span>{t('learnMore')}</span>
+                <ExternalLink />
+              </div>
+            </header>
+          </DescribeContainer>
+          <EligibilityList>
+            <Row>
+              <div>
+                <VerifiedIcon />
+                {t('airdrop.pool')}
+                <Tooltip title='Pools'>
+                  <QuestionIcon />
+                </Tooltip>
+              </div>
+              <span>{`0 ${t('lsd.symbol')}`}</span>
+            </Row>
+            <Row>
+              <div>
+                <ExcludeIcon />
+                {t('airdrop.locks')}
+                <Tooltip title='Pools'>
+                  <QuestionIcon />
+                </Tooltip>
+              </div>
+              <span>{`0 ${t('lsd.symbol')}`}</span>
+            </Row>
+          </EligibilityList>
+        </AirdropContainer>
+      </Container>
+      <WalletSidebarDisconnected />
+    </>
   )
 }
 
@@ -64,6 +91,7 @@ const {
   ExternalLink,
   EligibilityList,
   Row,
+  WalletConnectedContainer,
   VerifiedIcon,
   QuestionIcon,
   ExcludeIcon
@@ -105,11 +133,22 @@ const {
       font-weight: 400;
       line-height: normal;
     }
-  `,
-  WalletDisconnectedContainer: styled.div`
-    display: flex;
-    align-items: center;
-    gap: ${({ theme }) => theme.size[12]};
+
+    h4 {
+      font-size: ${({ theme }) => theme.font.size[14]};
+      color: ${({ theme }) => theme.color.blackAlpha[700]};
+      font-style: normal;
+      font-weight: 500;
+      line-height: normal;
+    }
+
+    span {
+      font-size: ${({ theme }) => theme.font.size[16]};
+      color: ${({ theme }) => theme.color.primary};
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+    }
 
     button {
       border-radius: ${props => props.theme.size[16]};
@@ -145,6 +184,31 @@ const {
         }
       }
     }
+  `,
+  WalletConnectedContainer: styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: ${({ theme }) => theme.size[24]};
+    border: 1px solid ${({ theme }) => theme.color.primary};
+    border-radius: ${({ theme }) => theme.size[16]};
+
+    > div {
+      display: flex;
+      flex-direction: column;
+      gap: ${({ theme }) => theme.size[8]};
+      > div {
+        display: flex;
+        gap: ${({ theme }) => theme.size[8]};
+        align-items: center;
+      }
+    }
+  `,
+  WalletDisconnectedContainer: styled.div`
+    display: flex;
+    align-items: center;
+    gap: ${({ theme }) => theme.size[12]};
   `,
   DescribeContainer: styled.div`
     width: 100%;
