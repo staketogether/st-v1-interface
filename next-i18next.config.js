56ada34fbea1297bce316801f5c0465f6311a92b
Arthur Ribeiro <arthurmr13@hotmail.com>
2023-08-07 19:37:07 -0300

    feat: remove en language

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,8 +2,8 @@ const path = require('path')
 
 module.exports = {
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'pt'],
+    defaultLocale: 'pt',
+    locales: ['pt'],
     localeDetection: true
   },
   trailingSlash: true,
