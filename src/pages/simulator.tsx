8c2afbf88f4f01a6d8b5224dd26a22b5810f3d15
Fabricio Miranda <fabriciomirandabr@gmail.com>
2023-08-27 18:40:40 -0300

    feat: update packages

--- src/pages/simulator.tsx
+++ src/pages/simulator.tsx
@@ -1,8 +1,6 @@
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-
 import { GetServerSideProps } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import LayoutTemplate from '../components/shared/layout/LayoutTemplate'
-
 import { Metatags } from '../components/shared/meta/Metatags'
 
 export default function Simulator() {
