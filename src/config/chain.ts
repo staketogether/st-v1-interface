fa0cd9f45a0e109729e2475b23ae75179012ae85
Fabricio Miranda <fabriciomirandabr@gmail.com>
2023-08-28 01:34:30 -0300

    fix: provider

--- src/config/chain.ts
+++ src/config/chain.ts
@@ -1,3 +1,5 @@
+import { ethers } from 'ethers'
+
 interface BlockExplorerConfig {
   baseUrl: string
 }
@@ -5,10 +7,11 @@ interface BlockExplorerConfig {
 export type ChainConfig = {
   chainId: number
   name: string
+  provider: ethers.JsonRpcProvider
   blockExplorer: BlockExplorerConfig
   subgraphs: {
     StakeTogether: string
-    contentful: string
+    ContentFul: string
   }
   contracts: {
     Airdrop: `0x${string}`
@@ -19,28 +22,16 @@ export type ChainConfig = {
 }
 
 const configs: ChainConfig[] = [
-  // {
-  //   chainId: 1,
-  //   name: 'mainnet',
-  //   provider: new ethers.providers.JsonRpcProvider(
-  //     `https://eth-goerli.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_MAINNET_API_KEY}`
-  //   ),
-  //   contracts: {
-  //     StakeTogether: '0x'
-  //   },
-  //   subgraphs: {
-  //     StakeTogether: '',
-  //     ENS: 'https://api.thegraph.com/subgraphs/name/ensdomains/ens'
-  //   }
-  // },
   {
     chainId: 5,
     name: 'goerli',
+    provider: new ethers.JsonRpcProvider(
+      `https://eth-goerli.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_GOERLI_API_KEY}`
+    ),
     blockExplorer: {
       baseUrl: 'https://goerli.etherscan.io'
     },
     contracts: {
-      // TODO: Update these addresses to the correct ones
       Airdrop: '0xd7200bb807b617E216fcB49A55Bfa86c57A3F6A7',
       Withdrawals: '0x9A1e2bc3068f56B3d737C022b8f5068Ea31DC63c',
       Router: '0x1F6AC1ee1bEB0C079164f45201ebf1424880Fd1b',
@@ -48,25 +39,13 @@ const configs: ChainConfig[] = [
     },
     subgraphs: {
       StakeTogether: 'https://api.studio.thegraph.com/query/17823/stake-together-v2-goerli/v1.2.1',
-      contentful: `https://graphql.contentful.com/content/v1/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_SPACE}/environments/master`
+      ContentFul: `https://graphql.contentful.com/content/v1/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_SPACE}/environments/master`
     }
   }
-  // {
-  //   chainId: 31337,
-  //   name: 'localhost',
-  //   provider: new ethers.providers.JsonRpcProvider(`http://127.0.0.1:8545`),
-  //   contracts: {
-  //     StakeTogether: '0x27c85150D755D383A4f86d0aeD8232815674771f'
-  //   },
-  //   subgraphs: {
-  //     StakeTogether: '',
-  //     ENS: ''
-  //   }
-  // }
 ]
 
 export default function chainConfig(): ChainConfig {
   const config = configs.find(c => c.chainId === Number(process.env.NEXT_PUBLIC_CHAIN_ID))
-  if (!config) throw new Error('chainId not found in global config')
+  if (!config) throw new Error('chainId not found in chainConfig')
   return config
 }
