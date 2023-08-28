607d98fb419345a774c9708398100c80d6672c50
Paulo Barcelos <paulo.barcelosti@gmail.com>
2023-08-28 19:04:02 -0300

    fix: activity

--- src/queries/subgraph/queryAccountActivities.ts
+++ src/queries/subgraph/queryAccountActivities.ts
@@ -2,7 +2,13 @@ import { gql } from '@apollo/client'
 
 export const queryAccountActivities = gql`
   query AccountActivities($accountAddress: ID!, $first: Int, $skip: Int) {
-    accountActivities(where: { account: $accountAddress }, first: $first, skip: $skip) {
+    accountActivities(
+      where: { account: $accountAddress }
+      first: $first
+      skip: $skip
+      orderBy: timestamp
+      orderDirection: desc
+    ) {
       timestamp
       type
       amount
