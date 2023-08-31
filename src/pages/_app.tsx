7287759679b50b9215022070d4cfad8ac06007fc
Paulo Barcelos <paulo.barcelosti@gmail.com>
2023-08-31 12:03:48 -0300

    feat: fix error on console

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { useMixpanelAnalytics } from '@/hooks/analytics/useMixpanelAnalytics'
 import { ApolloProvider } from '@apollo/client'
 import { appWithTranslation } from 'next-i18next'
 import type { AppProps } from 'next/app'
-import { Montserrat } from 'next/font/google'
+// import { Montserrat } from 'next/font/google'
 import { useRouter } from 'next/router'
 import NextNProgress from 'nextjs-progressbar'
 import { useEffect } from 'react'
@@ -19,7 +19,7 @@ import '../styles/reset.css'
 import { lightTheme } from '../styles/theme'
 import useSettingsCurrency from '@/hooks/useSettingCurrency'
 
-const montserrat = Montserrat({ subsets: ['latin'], weight: ['300', '400', '500'] })
+// const montserrat = Montserrat({ subsets: ['latin'], weight: ['300', '400', '500'] })
 
 const App = ({ Component, pageProps }: AppProps) => {
   validEnv()
@@ -38,7 +38,7 @@ const App = ({ Component, pageProps }: AppProps) => {
   }, [chain.chainId, registerPageView, router.events])
 
   return (
-    <div className={montserrat.className}>
+    <div>
       <GoogleTag />
       <Hotjar />
       <Cloudflare />
