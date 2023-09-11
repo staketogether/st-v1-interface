d28b2e0b5f166e89bbc328e93144fb14107d578f
Fabricio Miranda <fabriciomirandabr@gmail.com>
2023-09-11 20:13:13 -0300

    fix: url

--- src/config/global.ts
+++ src/config/global.ts
@@ -12,9 +12,9 @@ interface GlobalConfig {
 }
 
 export const globalConfig: GlobalConfig = {
-  url: 'https://beta.staketogether.app',
-  websiteUrl: 'https://beta.staketogether.app',
-  appUrl: 'https://beta.staketogether.app',
+  url: 'https://beta.staketogether.org',
+  websiteUrl: 'https://staketogether.org',
+  appUrl: 'https://beta.staketogether.org',
   auditUrl:
     'https://github.com/blocksecteam/audit-reports/blob/main/solidity/blocksec_staketogether_v1.1-signed.pdf',
   recaptchakey: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY as string,
