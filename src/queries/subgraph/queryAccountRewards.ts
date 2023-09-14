0487b6bd1c09d5e82026acdf7089088e498922b9
Paulo Barcelos <paulo.barcelosti@gmail.com>
2023-09-14 11:20:09 -0300

    feat: add ordering in the rewards sidebar

--- src/queries/subgraph/queryAccountRewards.ts
+++ src/queries/subgraph/queryAccountRewards.ts
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client'
 
 export const queryAccountRewards = gql`
   query AccountRewards($accountAddress: ID!) {
-    accountRewards(where: { account: $accountAddress }) {
+    accountRewards(where: { account: $accountAddress }, orderBy: timestamp, orderDirection: desc) {
       timestamp
       amount
       txHash
