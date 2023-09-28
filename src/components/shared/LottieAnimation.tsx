979bf877b8104d45793cab7407f6d3f3d6a052c3
Paulo Barcelos <46430156+paulobarcelosbr@users.noreply.github.com>
2023-09-28 18:18:53 -0300

    Feature/178 (#131)

---
+++ src/components/shared/LottieAnimation.tsx
@@ -0,0 +1,32 @@
+import React, { ReactNode } from 'react'
+import { Player } from '@lottiefiles/react-lottie-player'
+
+interface LottieAnimationProps {
+  animationData: object
+  loop?: boolean | number
+  autoplay?: boolean
+  children?: ReactNode
+  width?: number
+  height?: number
+}
+
+export default function LottieAnimation({
+  animationData,
+  loop = false,
+  autoplay = true,
+  width,
+  height
+}: LottieAnimationProps) {
+  return (
+    <>
+      <Player
+        autoplay={autoplay}
+        loop={loop}
+        controls={true}
+        src={animationData}
+        style={{ height: `${height ? `${height}px` : 'auto%'}`, maxWidth: `${width ? `${width}px` : '100%'}` }}
+        keepLastFrame={true}
+      />
+    </>
+  )
+}
