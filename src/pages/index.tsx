3499f81d0b9aa13fcd89fd624a444d304f48229c
Paulo Barcelos <paulo.barcelosti@gmail.com>
2023-10-09 17:16:37 -0300

    fix: fix build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import LayoutTemplate from '@/components/shared/layout/LayoutTemplate'
 import { Metatags } from '@/components/shared/meta/Metatags'
+import { GetServerSideProps } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import React from 'react'
 
 export default function Source() {
@@ -8,4 +10,12 @@ export default function Source() {
       <Metatags />
     </LayoutTemplate>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  return {
+    props: {
+      ...(await serverSideTranslations(context.locale || 'en', ['common']))
+    }
+  }
 }
