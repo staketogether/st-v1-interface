844fa3a91d75995b0b84b92d5a4d18dad8aff9b7
Paulo Barcelos <paulo.barcelosti@gmail.com>
2023-10-09 11:28:46 -0300

    feat: add metatags on _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,22 +1,17 @@
 import { Metatags } from '@/components/shared/meta/Metatags'
-import Document, { DocumentContext, DocumentInitialProps, Html } from 'next/document'
+
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
-export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+export default class AppDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: App => props =>
-            sheet.collectStyles(
-              <Html>
-                <Metatags />
-                <App {...props} />
-              </Html>
-            )
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
         })
 
       const initialProps = await Document.getInitialProps(ctx)
@@ -27,5 +22,38 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
+  }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name='title' content='Stake Together - Staking de Ethereum' />
+          <meta
+            name='description'
+            content='Invista em Ethereum com a Stake Together e maximize seus lucros enquanto apoia seus projetos favoritos através da Blockchain'
+          />
+          <meta property='og:type' content='website' />
+          <meta property='og:url' content='https://beta.staketogether.app' />
+          <meta property='og:title' content='Stake Together - Staking de Ethereum' />
+          <meta
+            property='og:description'
+            content='Invista em Ethereum com a Stake Together e maximize seus lucros enquanto apoia seus projetos favoritos através da Blockchain'
+          />
+          <meta property='og:image' content={`/assets/icons/stake-together/meta.png`} />
+          <meta property='twitter:card' content='summary_large_image' />
+          <meta property='twitter:url' content='https://beta.staketogether.app' />
+          <meta property='twitter:title' content='Stake Together - Staking de Ethereum' />
+          <meta
+            property='twitter:description'
+            content='Invista em Ethereum com a Stake Together e maximize seus lucros enquanto apoia seus projetos favoritos através da Blockchain'
+          />
+          <meta property='twitter:image' content={`/assets/icons/stake-together/meta.png`} />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
