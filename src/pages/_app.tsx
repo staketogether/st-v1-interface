c8f1656965e77dd6f69ee57f8ef57d90512185ab
Paulo Barcelos <paulo.barcelosti@gmail.com>
2023-10-09 21:46:42 -0300

    feat: remove pixel from app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,7 +14,6 @@ import validEnv from '../config/env'
 import { config } from '../config/wagmi'
 import '../styles/reset.css'
 import { lightTheme } from '../styles/theme'
-import Head from 'next/head'
 
 const App = ({ Component, pageProps }: AppProps) => {
   validEnv()
@@ -34,32 +33,6 @@ const App = ({ Component, pageProps }: AppProps) => {
 
   return (
     <>
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1367668057155824');
-              fbq('track', 'PageView');
-            `
-          }}
-        />
-        <noscript>
-          <img
-            height='1'
-            width='1'
-            style={{ display: 'none' }}
-            src='https://www.facebook.com/tr?id=1367668057155824&ev=PageView&noscript=1'
-          />
-        </noscript>
-      </Head>
       <ApolloProvider client={apolloClient}>
         <ThemeProvider theme={lightTheme}>
           <WagmiConfig config={config}>
