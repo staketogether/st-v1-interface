9137a9dc2a1d25893d3853e3fda97ac5240e989f
Fabricio Miranda <fabriciomirandabr@gmail.com>
2023-10-16 23:37:27 -0300

    feat: add strategy

--- src/components/shared/scripts/Intercom.tsx
+++ src/components/shared/scripts/Intercom.tsx
@@ -2,7 +2,7 @@ import Script from 'next/script'
 
 export const Intercom = () => {
   return (
-    <Script id='intercom-widget'>{`
+    <Script id='intercom-widget' strategy='afterInteractive'>{`
         (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/k6jidofs';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
     `}</Script>
   )
