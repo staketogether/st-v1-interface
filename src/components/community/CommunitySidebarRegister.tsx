46bd6af3d39ffb21b1cb0278f96c3cd1746d9745
Paulo Barcelos <paulo.barcelosti@gmail.com>
2023-10-18 13:35:18 -0300

    feat: fix

--- src/components/community/CommunitySidebarRegister.tsx
+++ src/components/community/CommunitySidebarRegister.tsx
@@ -1,39 +1,10 @@
-import styled from "styled-components"
+import styled from 'styled-components'
 
 export default function CommunitySidebarRegister() {
-  const { t } = useLocaleTranslation()
-
-  const router = useRouter()
-  const { currency, network } = router.query
-  const { setItem } = useLocalStorage()
-
-  const changeLocale = (newLocale: string) => {
-    router.push(router.pathname, router.asPath, { locale: newLocale })
-  }
-
-  const changeCurrency = (newCurrency: string) => {
-    router.push({
-      pathname: router.pathname,
-      query: { ...router.query, currency: newCurrency }
-    })
-
-    setItem('currency', newCurrency)
-  }
-
-  const changeNetwork = (newNetwork: string) => {
-    router.push({
-      pathname: router.pathname,
-      query: { ...router.query, network: newNetwork }
-    })
-    setItem('network', newNetwork)
-  }
-
-  return (
-   
-  )
+  // const { t } = useLocaleTranslation()
+  return <Container></Container>
 }
 
 const { Container } = {
-
   Container: styled.div``
 }
