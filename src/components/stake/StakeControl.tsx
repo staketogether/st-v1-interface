2edb65b80157007f31686d7a55b5508e59d9c925
Fabricio Miranda <fabriciomirandabr@gmail.com>
2023-12-04 10:38:38 -0300

    feat: remove unused vars

--- src/components/stake/StakeControl.tsx
+++ src/components/stake/StakeControl.tsx
@@ -118,13 +118,9 @@ export default function StakeControl({
     : t('v2.pages.deposit.description')
 
   const titleTvl = isStakeTogetherPool ? t('v2.stake.st.tvl') : t('v2.stake.tvl')
-  const titleRewards = isStakeTogetherPool ? t('v2.stake.st.rewards') : t('v2.stake.rewards')
   const titleApy = isStakeTogetherPool ? t('v2.stake.st.apy') : t('v2.stake.apy')
 
   const titleTvlTooltip = isStakeTogetherPool ? t('v2.stake.st.tvlTooltip') : t('v2.stake.tvlTooltip')
-  const titleRewardsTooltip = isStakeTogetherPool
-    ? t('v2.stake.st.rewardsTooltip')
-    : t('v2.stake.rewardsTooltip')
   const titleApyTooltip = isStakeTogetherPool ? t('v2.stake.st.apyTooltip') : t('v2.stake.apyTooltip')
 
   return (
