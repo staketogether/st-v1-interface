d4f6533f6829dd402b891c484a595938f6f5d535
Paulo Barcelos <paulo.barcelosti@gmail.com>
2023-12-18 14:25:47 -0300

    fix: regex

--- src/components/shared/regex.ts
+++ src/components/shared/regex.ts
@@ -3,8 +3,8 @@ export const projectRegexFields = {
   email: /^([A-Z|a-z|0-9](\.|_){0,1})+[A-Z|a-z|0-9]@([A-Z|a-z|0-9])+((\.){0,1}[A-Z|a-z|0-9]){2}\.[a-z]{2,3}$/,
   site: /[(https)://(www.)?a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_.~#?&=]*)/gi,
   socialMedia: /^[a-zA-Z0-9_]+$/,
-  discordInvite: /^https:\/\/discord\.com\/invite\/[a-zA-Z0-9\-_]+$/,
-  telegramInvite: /^https:\/\/(t\.me|telegram\.me)\/[a-zA-Z0-9_]+$/,
+  discordInvite: /^(https:\/\/)?discord(\.com\/invite\/|\.gg\/)[a-zA-Z0-9\-_]+$/,
+  telegramInvite: /^https:\/\/(t\.me|telegram\.me)\/.+$/,
   youtubeVideo: /^(https?:\/\/)?(www\.youtube\.com\/watch\?v=|youtu\.be\/)[\w-]+(&[\w-]+)*(&t=\d+s)?$/,
   youtubeChanel: /^@([a-zA-Z0-9\-.]+)$/
 }
