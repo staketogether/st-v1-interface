516472cca5e0ff38a99a83e2c82ee1dcaf486c24
Paulo Barcelos <paulo.barcelosti@gmail.com>
2024-01-16 18:16:42 -0300

    fix: main-net

--- src/config/web3Auth.ts
+++ src/config/web3Auth.ts
@@ -22,7 +22,7 @@ export default function Web3AuthConnectorInstance(chains: ChainConfig.Chain[]) {
   const web3AuthInstance = new Web3AuthNoModal({
     clientId: String(process.env.NEXT_PUBLIC_WEB3_AUTH_ID),
     chainConfig,
-    web3AuthNetwork: 'sapphire_devnet'
+    web3AuthNetwork: 'sapphire_mainnet'
   })
 
   const privateKeyProvider = new EthereumPrivateKeyProvider({ config: { chainConfig } })
