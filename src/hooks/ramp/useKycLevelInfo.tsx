a2047efe473071dfb015f4c4bd7f4cb811eb66e4
Arthur Ribeiro <arthurmr13@hotmail.com>
2024-02-08 16:42:43 -0300

    feat: added swr

---
+++ src/hooks/ramp/useKycLevelInfo.tsx
@@ -0,0 +1,21 @@
+import useSWR from 'swr'
+
+interface KycLevelInfo {
+  level: number
+  limits: {
+    limitBurn: number
+    limitMint: number
+    limitSwapBuy: number
+    limitSwapSell: number
+  }
+}
+
+export default function useKycLevelInfo(provider: 'brla' | 'transak', taxId: string) {
+  const { data, error } = useSWR<KycLevelInfo>(`api/ramp/kyc/${provider}/${taxId}/info`)
+
+  return {
+    kycLevelInfo: data,
+    isLoading: !error && !data,
+    isError: error
+  }
+}
