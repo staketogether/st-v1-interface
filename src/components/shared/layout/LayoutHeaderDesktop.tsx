fdd5af4bd33cd8ba6ba3ab4d6d52c7164c6641ee
Paulo Barcelos <46430156+paulobarcelosbr@users.noreply.github.com>
2024-02-12 11:07:09 -0300

    [ 1246 ] Add incentives alert  (#181)
    * fix: mobile redirect
    
    * feat: add incentives alert
    
    ---------
    
    Co-authored-by: Arthur <arthurmr13@hotmail.com>
    Co-authored-by: Fabricio Miranda <fabriciomirandabr@gmail.com>

--- src/components/shared/layout/LayoutHeaderDesktop.tsx
+++ src/components/shared/layout/LayoutHeaderDesktop.tsx
@@ -13,14 +13,20 @@ import stLogoDesktop from '../../../../public/assets/stake-together-desk.svg'
 import useActiveRoute from '../../../hooks/useActiveRoute'
 import useLocaleTranslation from '../../../hooks/useLocaleTranslation'
 import { ProjectButton } from '../../project/ProjectButton'
+import { useEffect, useState } from 'react'
+import { useLocalStorage } from '@/hooks/useLocalStorage'
 
 export default function LayoutHeader() {
+  const [showAlert, setShowAlert] = useState(false)
   const { t } = useLocaleTranslation()
   const { isActive } = useActiveRoute()
   const { account, accountIsConnected } = useConnectedAccount()
   const { setOpenProjectCreateModal } = useProjectCreateModal()
   const { query, isReady, pathname } = useRouter()
   const { currency, network } = query
+
+  const { getItem, setItem } = useLocalStorage()
+  const isShowAlert = getItem('showAlert')
 
   const { poolDetail: poolDetailUs } = useContentfulPoolDetails({
     poolAddress: account,
@@ -32,55 +38,79 @@ export default function LayoutHeader() {
   const withdrawBasePath = `/[network]/[currency]/withdraw`
   const isHome = (pathname === basePath || pathname === withdrawBasePath) && isReady
 
+  useEffect(() => {
+    if (!isShowAlert) setShowAlert(true)
+  }, [isShowAlert])
+
   return (
-    <Container>
-      <MenuContainer>
-        <div>
-          <Logo href={`/${network}/${currency}`}>
-            <Image src={stLogoDesktop} alt={t('stakeTogether')} width={162} height={27} />
-          </Logo>
-        </div>
-        <Menu>
-          <Link href={`/${network}/${currency}`}>
-            <MenuButton className={`${isHome ? 'active' : ''}`}>
-              <InvestIcon />
-              {t('v2.header.stake')}
-            </MenuButton>
-          </Link>
-          <Link href={`/${network}/${currency}/project`}>
-            <MenuButton
-              className={`${
-                !isHome && (isActive('project') || isActive('deposit') || isActive('withdraw')) ? 'active' : ''
-              }`}
-            >
-              <ProjectsIcon />
-              {t('v2.header.projects')}
-            </MenuButton>
-          </Link>
-          <Link href={`/${network}/${currency}/analytics`}>
-            <MenuButton className={`${!isHome && isActive('analytics') ? 'active' : ''}`}>
-              <AnalyticsIcon />
-              {t('v2.header.analytics')}
-            </MenuButton>
-          </Link>
-          {/* <Link href={`/${network}/${currency}/incentives`}>
+    <HeaderContainer>
+      <Container>
+        <MenuContainer>
+          <div>
+            <Logo href={`/${network}/${currency}`}>
+              <Image src={stLogoDesktop} alt={t('stakeTogether')} width={162} height={27} />
+            </Logo>
+          </div>
+          <Menu>
+            <Link href={`/${network}/${currency}`}>
+              <MenuButton className={`${isHome ? 'active' : ''}`}>
+                <InvestIcon />
+                {t('v2.header.stake')}
+              </MenuButton>
+            </Link>
+            <Link href={`/${network}/${currency}/project`}>
+              <MenuButton
+                className={`${
+                  !isHome && (isActive('project') || isActive('deposit') || isActive('withdraw'))
+                    ? 'active'
+                    : ''
+                }`}
+              >
+                <ProjectsIcon />
+                {t('v2.header.projects')}
+              </MenuButton>
+            </Link>
+            <Link href={`/${network}/${currency}/analytics`}>
+              <MenuButton className={`${!isHome && isActive('analytics') ? 'active' : ''}`}>
+                <AnalyticsIcon />
+                {t('v2.header.analytics')}
+              </MenuButton>
+            </Link>
+            {/* <Link href={`/${network}/${currency}/incentives`}>
             <MenuButton className={`${isActive('incentives') ? 'active' : ''}`}>
               <IncentivesIcon /> {t('v2.header.incentives')}
             </MenuButton>
           </Link> */}
-        </Menu>
-      </MenuContainer>
-      <WalletContainer>
-        {!poolDetailUs && (
-          <MenuButton onClick={() => setOpenProjectCreateModal(true)}>
-            <CreateProjectIcon /> {t('v2.createProject.title')}
-          </MenuButton>
-        )}
-        {poolDetailUs && <ProjectButton poolDetail={poolDetailUs} account={account} />}
-        <Wallet account={account} accountIsConnected={accountIsConnected} />
-      </WalletContainer>
-      {screenWidth > breakpoints.lg && <ProjectCreateModal account={account} poolDetail={poolDetailUs} />}
-    </Container>
+          </Menu>
+        </MenuContainer>
+        <WalletContainer>
+          {!poolDetailUs && (
+            <MenuButton onClick={() => setOpenProjectCreateModal(true)}>
+              <CreateProjectIcon /> {t('v2.createProject.title')}
+            </MenuButton>
+          )}
+          {poolDetailUs && <ProjectButton poolDetail={poolDetailUs} account={account} />}
+          <Wallet account={account} accountIsConnected={accountIsConnected} />
+        </WalletContainer>
+        {screenWidth > breakpoints.lg && <ProjectCreateModal account={account} poolDetail={poolDetailUs} />}
+      </Container>
+      {showAlert && (
+        <AlertBar>
+          <div>
+            <span className='bold'>{t('v2.notifyBar.incentives.title')}</span>
+            <span>{t('v2.notifyBar.incentives.description')}</span>
+          </div>
+          <CloseButton
+            onClick={() => {
+              setItem('showAlert', 'incentives')
+              setShowAlert(false)
+            }}
+          >
+            <span>x</span>
+          </CloseButton>
+        </AlertBar>
+      )}
+    </HeaderContainer>
   )
 }
 
@@ -94,23 +124,31 @@ const {
   InvestIcon,
   ProjectsIcon,
   AnalyticsIcon,
-  CreateProjectIcon
+  CreateProjectIcon,
+  CloseButton,
+  AlertBar,
+  HeaderContainer
 } = {
-  Container: styled.header`
+  Container: styled.div`
     display: none;
     gap: ${({ theme }) => theme.size[32]};
     background: ${({ theme }) => theme.colorV2.white};
     box-shadow: ${({ theme }) => theme.shadow[100]};
     padding: ${props => props.theme.size[16]} ${props => props.theme.size[24]};
+
+    @media (min-width: ${({ theme }) => theme.breakpoints.lg}) {
+      display: grid;
+      grid-template-columns: 1fr auto;
+    }
+  `,
+  HeaderContainer: styled.header`
     position: fixed;
     left: 0;
     top: 0;
     z-index: 5;
     width: 100%;
-    @media (min-width: ${({ theme }) => theme.breakpoints.lg}) {
-      display: grid;
-      grid-template-columns: 1fr auto;
-    }
+    display: flex;
+    flex-direction: column;
   `,
   MenuContainer: styled.div`
     display: flex;
@@ -185,5 +223,45 @@ const {
   `,
   AnalyticsIcon: styled(PiChartLine)`
     font-size: 15px;
+  `,
+  AlertBar: styled.div`
+    background: ${({ theme }) => theme.colorV2.purple[1]};
+    padding: ${({ theme }) => theme.size[12]} ${({ theme }) => theme.size[24]};
+
+    font-size: 13px;
+    font-weight: 500;
+    color: ${({ theme }) => theme.colorV2.white};
+
+    display: grid;
+    grid-template-columns: 1fr auto;
+
+    > div {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: ${({ theme }) => theme.size[4]};
+    }
+
+    span {
+      text-align: center;
+      &.bold {
+        font-weight: 700;
+        text-transform: uppercase;
+      }
+    }
+  `,
+  CloseButton: styled.button`
+    transition: background 0.2s ease;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-left: auto;
+    background: transparent;
+    border: none;
+    span {
+      font-size: ${({ theme }) => theme.font.size[18]};
+      color: ${({ theme }) => theme.colorV2.white};
+      font-weight: 100;
+    }
   `
 }
