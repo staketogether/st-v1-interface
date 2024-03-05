be71bb367d3048ad59b888b406a0d1305bc46aaf
Paulo Barcelos <paulo.barcelosti@gmail.com>
2024-03-05 15:33:50 -0300

    feat: add div for mark screen

--- src/components/shared/TradingViewComponent.tsx
+++ src/components/shared/TradingViewComponent.tsx
@@ -18,20 +18,22 @@ export default function TradingViewComponent() {
   }
 
   return (
-    <AdvancedRealTimeChart
-      symbol={currencies[currency as keyof typeof currencies]}
-      hide_side_toolbar
-      hide_top_toolbar
-      hide_legend
-      height={320}
-      calendar={false}
-      range='12M'
-      interval='1'
-      width={'100%'}
-      theme='light'
-      copyrightStyles={{ parent: { display: 'none' }, link: { background: 'red' } }}
-      style='3'
-      disabled_features={['create_volume_indicator_by_default']}
-    />
+    <div style={{ width: '100%', height: '320px' }}>
+      <AdvancedRealTimeChart
+        symbol={currencies[currency as keyof typeof currencies]}
+        hide_side_toolbar
+        hide_top_toolbar
+        hide_legend
+        height={320}
+        calendar={false}
+        range='12M'
+        interval='1'
+        width={'100%'}
+        theme='light'
+        copyrightStyles={{ parent: { display: 'none' }, link: { background: 'red' } }}
+        style='3'
+        disabled_features={['create_volume_indicator_by_default']}
+      />
+    </div>
   )
 }
