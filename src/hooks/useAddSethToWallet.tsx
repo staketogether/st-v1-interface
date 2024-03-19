75461766f422e6446d8868cabb354746bc217907
Paulo Barcelos <paulo.barcelosti@gmail.com>
2024-03-19 16:43:31 -0300

    fix: fix

--- src/hooks/useAddSethToWallet.tsx
+++ src/hooks/useAddSethToWallet.tsx
@@ -9,12 +9,26 @@ type useAddSethToWalletProps = {
 export default function useAddSethToWallet({ productSymbol, contractAddress }: useAddSethToWalletProps) {
   const { connector, isConnected } = useAccount()
 
+  const productSymbolIcons = {
+    stpETH: 'https://raw.githubusercontent.com/staketogether/st-v1-interface/dev/public/assets/st-icon.png',
+    stpRETH:
+      'https://raw.githubusercontent.com/staketogether/st-v1-interface/faa3dcdbf60ea06f922c3c9dd1ed1d8f20fa1cbb/public/assets/stpRETHIcon.svg',
+    stpPOL: 'https://raw.githubusercontent.com/staketogether/st-v1-interface/dev/public/assets/st-icon.png',
+    stpSOL: 'https://raw.githubusercontent.com/staketogether/st-v1-interface/dev/public/assets/st-icon.png',
+    stpTIA: 'https://raw.githubusercontent.com/staketogether/st-v1-interface/dev/public/assets/st-icon.png',
+    stpNear: 'https://raw.githubusercontent.com/staketogether/st-v1-interface/dev/public/assets/st-icon.png',
+    stpKSM: 'https://raw.githubusercontent.com/staketogether/st-v1-interface/dev/public/assets/st-icon.png',
+    stpATOM: 'https://raw.githubusercontent.com/staketogether/st-v1-interface/dev/public/assets/st-icon.png',
+    stpBTC: 'https://raw.githubusercontent.com/staketogether/st-v1-interface/dev/public/assets/st-icon.png',
+    stpCHZ: 'https://raw.githubusercontent.com/staketogether/st-v1-interface/dev/public/assets/st-icon.png'
+  }
+
   const addToWalletAction = () => {
     if (connector && isConnected) {
       connector.watchAsset?.({
         address: contractAddress,
         symbol: productSymbol,
-        image: 'https://raw.githubusercontent.com/staketogether/st-v1-interface/dev/public/assets/st-icon.png',
+        image: productSymbolIcons[productSymbol],
         decimals: 18
       })
     }
