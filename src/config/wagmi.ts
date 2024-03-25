70f6a7290c1aaf44b7f29ee493cebfe815afd603
Arthur RIbeiro <arthurmr13@hotmail.com>
2024-03-25 12:02:38 -0300

    feat: updated web3auth integration

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -1,58 +1,62 @@
-import { holesky, mainnet, optimismSepolia } from 'viem/chains'
-import { configureChains, createConfig } from 'wagmi'
-import { InjectedConnector } from 'wagmi/connectors/injected'
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
-import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
-import Web3AuthConnectorInstance from './web3Auth'
+import { createConfig, http } from 'wagmi'
+import { mainnet, optimism, holesky, optimismSepolia } from 'wagmi/chains'
+import { injected, walletConnect, safe } from 'wagmi/connectors'
+import { defineChain, Chain } from 'viem'
+import Web3AuthConnectorInstances from '@/config/web3Auth'
 
 const handleRpcPerChain = (chainId: number) => {
   const alchemyKey: { [key: number]: string } = {
     1: process.env.NEXT_PUBLIC_RPC_MAINNET_URL as string,
+    10: process.env.NEXT_PUBLIC_RPC_OPTIMISM_URL as string,
     17000: process.env.NEXT_PUBLIC_RPC_HOLESKY_URL as string,
-    11155420: process.env.NEXT_PUBLIC_RPC_OPTIMIST_SEPOLIA_URL as string
+    11155420: process.env.NEXT_PUBLIC_RPC_OPTIMISM_SEPOLIA_URL as string
   }
 
   return alchemyKey[chainId] || ''
 }
 
-const { chains, publicClient } = configureChains(
-  [optimismSepolia, holesky, mainnet],
-  [
-    jsonRpcProvider({
-      rpc: chain => ({
-        http: handleRpcPerChain(chain.id),
-        chainId: chain.id
-      })
-    })
-  ],
-  {
-    retryCount: 1
+const mainnetChain = defineChain({
+  ...mainnet,
+  rpcUrls: {
+    default: { http: [process.env.NEXT_PUBLIC_RPC_MAINNET_URL as string] },
   }
-)
+})
 
-const connectors = [
-  ...Web3AuthConnectorInstance(chains),
-  new MetaMaskConnector({ chains }),
-  new InjectedConnector({
-    chains,
-    options: {
-      name: 'Rabby'
-    }
-  }),
-  new WalletConnectConnector({
-    chains,
-    options: {
-      projectId: String(process.env.NEXT_PUBLIC_WALLET_CONNECT),
-      showQrModal: true
-    }
-  })
-]
+const optimismChain = defineChain({
+  ...optimism,
+  rpcUrls: {
+    default: { http: [process.env.NEXT_PUBLIC_RPC_OPTIMISM_URL as string] },
+  }
+})
+
+const holeskyChain = defineChain({
+  ...holesky,
+  rpcUrls: {
+    default: { http: [process.env.NEXT_PUBLIC_RPC_HOLESKY_URL as string] },
+  }
+})
+
+const optimismSepoliaChain = defineChain({
+  ...optimismSepolia,
+  rpcUrls: {
+    default: { http: [process.env.NEXT_PUBLIC_RPC_OPTIMISM_SEPOLIA_URL as string] },
+  }
+})
+
+const chains: [Chain, ...Chain[]] = process.env.NEXT_PUBLIC_ENV === 'production' ? [mainnetChain, optimismChain] : [holeskyChain, optimismSepoliaChain]
+
+const web3Auth = () => Web3AuthConnectorInstances({ chains })
 
 const config = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient
+  chains,
+  connectors: [...web3Auth(), injected(), safe(), walletConnect({ projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT as string})],
+  transports: process.env.NEXT_PUBLIC_ENV === 'production' ? {
+    [mainnet.id]: http(handleRpcPerChain(mainnet.id)),
+    [optimism.id]: http(handleRpcPerChain(optimism.id))
+  } : {
+    [holesky.id]: http(handleRpcPerChain(holesky.id)),
+    [optimismSepolia.id]: http(handleRpcPerChain(optimismSepolia.id))
+  },
 })
 
 export { chains, config }
--- src/config/web3Auth.ts
+++ src/config/web3Auth.ts
@@ -1,24 +1,26 @@
-import { CHAIN_NAMESPACES } from '@web3auth/base'
-import { EthereumPrivateKeyProvider } from '@web3auth/ethereum-provider'
-import { Web3AuthNoModal } from '@web3auth/no-modal'
-import { OpenloginAdapter } from '@web3auth/openlogin-adapter'
-import { Web3AuthConnector } from '@web3auth/web3auth-wagmi-connector'
-import * as ChainConfig from 'viem/chains'
+import { Web3AuthConnector } from "@web3auth/web3auth-wagmi-connector";
+import { Web3AuthNoModal } from "@web3auth/no-modal";
+import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
+import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
+import { CHAIN_NAMESPACES, UX_MODE } from "@web3auth/base";
+import { Chain } from "wagmi/chains";
 import stSymbol from '@assets/st-symbol.svg'
+import { CreateConnectorFn } from 'wagmi'
 
 const iconUrl = stSymbol
 
 const handleRpcPerChain = (chainId: number) => {
   const alchemyKey: { [key: number]: string } = {
     1: process.env.NEXT_PUBLIC_RPC_MAINNET_URL as string,
+    10: process.env.NEXT_PUBLIC_RPC_OPTIMISM_URL as string,
     17000: process.env.NEXT_PUBLIC_RPC_HOLESKY_URL as string,
-    11155420: process.env.NEXT_PUBLIC_RPC_OPTIMIST_SEPOLIA_URL as string
+    11155420: process.env.NEXT_PUBLIC_RPC_OPTIMISM_SEPOLIA_URL as string
   }
 
   return alchemyKey[chainId] || ''
 }
 
-export default function Web3AuthConnectorInstance(chains: ChainConfig.Chain[]) {
+export default function Web3AuthConnectorInstances({chains}:{chains: [Chain, ...Chain[]]}): CreateConnectorFn[] {
   const chainConfig = {
     chainNamespace: CHAIN_NAMESPACES.EIP155,
     chainId: '0x' + chains[0].id.toString(16),
@@ -29,21 +31,20 @@ export default function Web3AuthConnectorInstance(chains: ChainConfig.Chain[]) {
     blockExplorer: chains[0].blockExplorers?.default.url[0] as string
   }
 
+  const privateKeyProvider = new EthereumPrivateKeyProvider({ config: { chainConfig } });
+
   const web3AuthInstance = new Web3AuthNoModal({
     clientId: chains[0].testnet
       ? String(process.env.NEXT_PUBLIC_WEB3_DEVNET_AUTH_ID)
       : String(process.env.NEXT_PUBLIC_WEB3_AUTH_ID),
-    chainConfig,
+    privateKeyProvider,
     web3AuthNetwork: chains[0].testnet ? 'sapphire_devnet' : 'sapphire_mainnet'
   })
 
-  const privateKeyProvider = new EthereumPrivateKeyProvider({ config: { chainConfig } })
-
   const openloginAdapterInstance = new OpenloginAdapter({
-    privateKeyProvider,
+    web3AuthNetwork: chains[0].testnet ? 'testnet' : 'mainnet',
     adapterSettings: {
-      network: chains[0].testnet ? 'testnet' : 'mainnet',
-      uxMode: 'redirect',
+      uxMode: UX_MODE.REDIRECT,
       whiteLabel: {
         logoLight: iconUrl,
         logoDark: iconUrl,
@@ -53,33 +54,26 @@ export default function Web3AuthConnectorInstance(chains: ChainConfig.Chain[]) {
   })
   web3AuthInstance.configureAdapter(openloginAdapterInstance)
 
-  return [
-    new Web3AuthConnector({
-      chains: chains,
-      options: {
-        web3AuthInstance,
-        loginParams: {
-          loginProvider: 'google'
-        }
-      }
-    }),
-    new Web3AuthConnector({
-      chains: chains,
-      options: {
-        web3AuthInstance,
-        loginParams: {
-          loginProvider: 'facebook'
-        }
-      }
-    }),
-    new Web3AuthConnector({
-      chains: chains,
-      options: {
-        web3AuthInstance,
-        loginParams: {
-          loginProvider: 'apple'
-        }
-      }
-    })
-  ]
+  const googleConnector: CreateConnectorFn = new Web3AuthConnector({
+    web3AuthInstance,
+    loginParams: {
+      loginProvider: 'google'
+    }
+  })
+
+  const facebookConnector: CreateConnectorFn = new Web3AuthConnector({
+    web3AuthInstance,
+    loginParams: {
+      loginProvider: 'facebook'
+    }
+  })
+
+  const appleConnector: CreateConnectorFn = new Web3AuthConnector({
+    web3AuthInstance,
+    loginParams: {
+      loginProvider: 'apple'
+    }
+  })
+
+  return [googleConnector, facebookConnector, appleConnector]
 }
