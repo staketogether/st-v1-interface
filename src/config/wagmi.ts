0c6d9247dcfd67e56af6ab224bafb8ec438d7eb2
Paulo Barcelos <paulo.barcelosti@gmail.com>
2024-04-01 09:04:04 -0300

    fix: build pages

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -3,13 +3,20 @@ import { mainnet, optimism, holesky, optimismSepolia } from 'wagmi/chains'
 import { safe, walletConnect } from 'wagmi/connectors'
 import Web3AuthConnectorInstances from '@/config/web3Auth'
 
+const handleConnectors = () => {
+  if (typeof window !== 'undefined') {
+    return [
+      ...Web3AuthConnectorInstances([mainnet, optimismSepolia, holesky]).connectors,
+      safe(),
+      walletConnect({ projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT as string, showQrModal: true })
+    ]
+  }
+  return []
+}
+
 export const config = createConfig({
   chains: [mainnet, optimism, holesky, optimismSepolia],
-  connectors: [
-    ...Web3AuthConnectorInstances([mainnet, optimismSepolia, holesky]).connectors,
-    safe(),
-    walletConnect({ projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT as string, showQrModal: true })
-  ],
+  connectors: handleConnectors(),
   transports: {
     [mainnet.id]: http(process.env.NEXT_PUBLIC_ALCHEMY_MAINNET_API_KEY as string),
     [optimismSepolia.id]: http(process.env.NEXT_PUBLIC_ALCHEMY_OPTIMIST_SEPOLIA_API_KEY as string),
