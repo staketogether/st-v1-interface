7f873fcf28c604674648f4e8fca3e13fad19bd98
Fabricio Miranda <fabriciomirandabr@gmail.com>
2024-04-02 16:31:08 +0300

    fix: terms

--- src/config/global.ts
+++ src/config/global.ts
@@ -3,19 +3,16 @@ interface GlobalConfig {
   websiteUrl: string
   auditUrl: string
   appUrl: string
-  apy: string
-  stakeTogetherUniversityUrlBr: string
-  stakeTogetherUniversityUrlEn: string
   docsPt: string
   docsEn: string
+  termsPt: string
+  termsEn: string
+  privacyPt: string
+  privacyEn: string
   backendUrl: string
   contentFul: string
   analyticsSubgraph: string
   backendSubgraph: string
-  fees: {
-    operation: string
-    rewards: string
-  }
 }
 
 export const globalConfig: GlobalConfig = {
@@ -23,17 +20,14 @@ export const globalConfig: GlobalConfig = {
   websiteUrl: 'https://staketogether.org',
   appUrl: 'https://app.staketogether.org',
   auditUrl: 'https://github.com/staketogether/st-v1-contracts/tree/main/audits',
-  stakeTogetherUniversityUrlEn: 'https://university.staketogether.org/en/collections/6550996-documentation',
-  stakeTogetherUniversityUrlBr: 'https://university.staketogether.org/pt/collections/6550996-documentation',
-  docsPt: 'https://docs.staketogether.org',
-  docsEn: 'https://docs.staketogether.org/stake-together/v/stake-together-en/stake-together/what-we-do',
+  docsPt: 'https://docs.staketogether.org/protocol/v/portugues',
+  docsEn: 'https://docs.staketogether.org/protocol',
+  termsPt: 'https://docs.staketogether.org/protocol/v/portugues/juridico/termos-de-uso',
+  termsEn: 'https://docs.staketogether.org/protocol/legal/terms-and-conditions',
+  privacyPt: 'https://docs.staketogether.org/protocol/v/portugues/juridico/politica-de-privacidade',
+  privacyEn: 'https://docs.staketogether.org/protocol/legal/privacy-policies',
   backendUrl: process.env.NEXT_PUBLIC_BACKEND_URL ?? 'https://st-backend-dev-thz2yhu72a-ue.a.run.app',
   contentFul: `https://graphql.contentful.com/content/v1/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_SPACE}/environments/${process.env.NEXT_PUBLIC_CONTENTFUL_ENVIRONMENT}`,
   analyticsSubgraph: 'https://st-analytics-api-ddfui.ondigitalocean.app/graphql',
- backendSubgraph: `${process.env.NEXT_PUBLIC_BACKEND_URL ?? 'https://st-backend-dev-thz2yhu72a-ue.a.run.app'}/graphql`,
-  apy: '5.1',
-  fees: {
-    operation: '0',
-    rewards: '8'
-  }
+  backendSubgraph: `${process.env.NEXT_PUBLIC_BACKEND_URL ?? 'https://st-backend-dev-thz2yhu72a-ue.a.run.app'}/graphql`
 }
