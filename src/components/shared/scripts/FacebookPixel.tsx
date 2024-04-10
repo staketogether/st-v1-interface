7ff959e9e037a6670c1c6c7100bb6dedff6c54db
Paulo Barcelos <paulo.barcelosti@gmail.com>
2024-04-10 16:36:16 -0300

    fix: fix

--- src/components/shared/scripts/FacebookPixel.tsx
+++ src/components/shared/scripts/FacebookPixel.tsx
@@ -15,16 +15,16 @@ export const FacebookPixel = ({ eventTrack }: FacebookPixelProps) => {
         strategy='afterInteractive'
         dangerouslySetInnerHTML={{
           __html: `
-          !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '788990793167571');
-        fbq('track', ${eventTrack});
+        !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '788990793167571');
+      fbq('track', ${eventTrack});
       `
         }}
       />
@@ -33,7 +33,7 @@ export const FacebookPixel = ({ eventTrack }: FacebookPixelProps) => {
           height='1'
           width='1'
           style={{ display: 'none', height: 1, width: 1 }}
-          src='https://www.facebook.com/tr?id=1367668057155824&ev=PageView&noscript=1'
+          src='https://www.facebook.com/tr?id=788990793167571&ev=PageView&noscript=1'
         />
       </noscript>
     </>
@@ -41,5 +41,5 @@ export const FacebookPixel = ({ eventTrack }: FacebookPixelProps) => {
 }
 
 const { PixelImage } = {
-  PixelImage: styled.img``
+  PixelImage: styled.div``
 }
