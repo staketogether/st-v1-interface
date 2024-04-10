e8f20da9faa52b7692d403dd2ef9c457685443ea
Paulo Barcelos <paulo.barcelosti@gmail.com>
2024-04-10 16:00:01 -0300

    fix: fix

--- src/components/shared/scripts/FacebookPixel.tsx
+++ src/components/shared/scripts/FacebookPixel.tsx
@@ -15,7 +15,7 @@ export const FacebookPixel = ({ eventTrack }: FacebookPixelProps) => {
         strategy='afterInteractive'
         dangerouslySetInnerHTML={{
           __html: `
-        !function(f,b,e,v,n,t,s)
+          !function(f,b,e,v,n,t,s)
         {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
         n.callMethod.apply(n,arguments):n.queue.push(arguments)};
         if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -23,8 +23,8 @@ export const FacebookPixel = ({ eventTrack }: FacebookPixelProps) => {
         t.src=v;s=b.getElementsByTagName(e)[0];
         s.parentNode.insertBefore(t,s)}(window, document,'script',
         'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '1367668057155824');
-        fbq('track', '${eventTrack}');
+        fbq('init', '788990793167571');
+        fbq('track', ${eventTrack});
       `
         }}
       />
