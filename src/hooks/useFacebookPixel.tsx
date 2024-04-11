6ee328f1b9194a24f1b08f218e4e4d0f25dcd610
Paulo Barcelos <paulo.barcelosti@gmail.com>
2024-04-11 15:03:27 -0300

    fix: add ts-ignore to fix typescript error

--- src/hooks/useFacebookPixel.tsx
+++ src/hooks/useFacebookPixel.tsx
@@ -3,9 +3,9 @@ import { useEffect } from 'react'
 export const useFacebookPixel = (eventTrack: string, isTracked = true) => {
   useEffect(() => {
     function fbqTrackEvent() {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      //@ts-ignore
       if ((window as Window).fbq !== undefined) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        //@ts-ignore
         window.fbq('track', eventTrack)
       }
     }
