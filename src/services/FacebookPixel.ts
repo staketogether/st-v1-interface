6c78460efd5ab74205b17307f9faa31a4281e0fb
Paulo Barcelos <paulo.barcelosti@gmail.com>
2024-04-11 15:00:09 -0300

    fix: ignore facebook pixel

--- src/services/FacebookPixel.ts
+++ src/services/FacebookPixel.ts
@@ -1,4 +1,6 @@
 export function fbqTrackEvent(eventTrack: string) {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  //@ts-ignore
   if ((window as Window).fbq !== undefined) {
     window.fbq('track', eventTrack)
   }
