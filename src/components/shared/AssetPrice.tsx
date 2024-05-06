06bbe87e22af8b9060fe6948937f9dfe90cd3147
Arthur RIbeiro <arthurmr13@hotmail.com>
2024-05-06 10:30:45 -0300

    fix: show loading when the price is loading

--- src/components/shared/AssetPrice.tsx
+++ src/components/shared/AssetPrice.tsx
@@ -10,5 +10,5 @@ interface AssetPriceProps {
 export default function AssetPrice({ asset, className }: AssetPriceProps) {
   const contractAddress = asset.type === 'native' ? asset.wrapperContractAddress : asset.contractAddress
   const { priceConvertedValue, loading } = useCoinConversion('1', asset.chains[0], contractAddress)
-  return loading && !priceConvertedValue ? <SkeletonLoading width={80} /> : <span className={className}>{priceConvertedValue}</span>
+  return loading ? <SkeletonLoading width={80} /> : <span className={className}>{priceConvertedValue}</span>
 }
--- src/hooks/useGetCurrencyPrice.tsx
+++ src/hooks/useGetCurrencyPrice.tsx
@@ -1,14 +1,16 @@
 import { assetsList } from '@/config/product/asset'
 import { globalConfig } from '@/config/global'
-import { makeVar } from '@apollo/client'
+import { makeVar, useReactiveVar } from '@apollo/client'
 import axios from 'axios'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { AssetStats } from '@/types/AssetStats'
 
 export const currencyPriceListVar = makeVar<{ id: string; value: number; price24h: number }[]>([])
 
 export default function useGetCurrencyPrice() {
   const { backendUrl } = globalConfig
+  const [loading, setLoading] = useState(true)
+  const currencyPriceList = useReactiveVar(currencyPriceListVar)
 
   useEffect(() => {
     const getDataPromise = async () => {
@@ -30,10 +32,14 @@ export default function useGetCurrencyPrice() {
         })
 
         currencyPriceListVar(responseData)
+        setLoading(false)
       } catch (error) {
         console.error('Error Quotation price:', error)
+        setLoading(false)
       }
     }
     getDataPromise()
   }, [backendUrl])
+
+  return { isLoading: loading, currencyPriceList }
 }
