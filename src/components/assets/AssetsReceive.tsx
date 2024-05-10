47751a3cae23053bdbde4ec545b5a264d4be1bca
Paulo Barcelos <paulo.barcelosti@gmail.com>
2024-05-10 16:56:23 -0300

    feat: add receive container

--- src/components/assets/AssetsReceive.tsx
+++ src/components/assets/AssetsReceive.tsx
@@ -1,50 +1,60 @@
-import useLocaleTranslation from "@/hooks/useLocaleTranslation"
-import { PiArrowRight } from "react-icons/pi"
-import styled from "styled-components"
-import Button from "../shared/Button"
+import useLocaleTranslation from '@/hooks/useLocaleTranslation'
+import styled from 'styled-components'
+import Button from '../shared/Button'
+import { notification } from 'antd'
+import QRCode from 'react-qr-code'
+import { useAccount } from 'wagmi'
+import { FiCopy } from 'react-icons/fi'
 
+export function AssetsReceive() {
+  const { t } = useLocaleTranslation()
+  const account = useAccount()
 
-import { Asset } from "@/types/Asset"
-import { notification } from "antd"
-import QRCode from "react-qr-code"
-import { useAccount } from "wagmi"
-
-interface AssetSendProps {
-    walletTo: string
-    amountToken: string
+  const handleCopyClipboard = () => {
+    navigator.clipboard.writeText(account?.address ?? '')
+    notification.success({
+      message: t('v2.ramp.copyCodeSuccess'),
+      placement: 'topRight'
+    })
+  }
+  return (
+    <Container>
+      <Code value={account?.address ?? ''} />
+      <WalletContainer>
+        <span>{account?.address ?? ''}</span>
+      </WalletContainer>
+      <Button label={t('copyWalletAddress')} icon={<FiCopy />} block onClick={handleCopyClipboard} />
+    </Container>
+  )
 }
 
-export function AssetsReceive({ asset }: { asset: Asset }) {
-    const { t } = useLocaleTranslation()
-    const account = useAccount()
+const { Container, Code, WalletContainer } = {
+  Container: styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: ${({ theme }) => theme.size[32]};
+  `,
+  Code: styled(QRCode)`
+    border: none;
+    width: 160px !important;
+    height: 160px !important;
+  `,
+  WalletContainer: styled.div`
+    width: 100%;
+    background: ${({ theme }) => theme.colorV2.foreground};
+    border-radius: ${({ theme }) => theme.size[8]};
+    padding: ${({ theme }) => theme.size[8]};
 
-    const handleCopyClipboard = () => {
-        navigator.clipboard.writeText(account?.address ?? '')
-        notification.success({
-            message: t('v2.ramp.copyCodeSuccess'),
-            placement: 'topRight'
-        })
-    }
-    return (
-        <FormContainer >
-            <Code value={account?.address ?? ''} />
-            <span>{account?.address ?? ''}</span>
-            <Button form='assetSendForm' label={t('next')} icon={<PiArrowRight />} onClick={handleCopyClipboard} />
-        </FormContainer>
-    )
-}
-
-const { FormContainer, Code } = {
-    FormContainer: styled.div`
+    span {
+      width: 100%;
+      text-align: center;
+      font-size: ${({ theme }) => theme.font.size[20]};
+      color: ${({ theme }) => theme.colorV2.blue[1]};
+      word-break: break-all;
       display: flex;
-      flex-direction: column;
-      gap: ${({ theme }) => theme.size[8]};
-      max-height: 450px;
-      max-width: 420px;
-    `,
-    Code: styled(QRCode)`
-     border: none;
-     width: 160px !important;
-     height: 160px !important;
-   `,
+      align-items: center;
+    }
+  `
 }
