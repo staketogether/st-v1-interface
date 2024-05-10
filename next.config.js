bbce61e2d9482c138e71de9bdd53a6b830129054
Arthur RIbeiro <arthurmr13@hotmail.com>
2024-05-10 17:45:23 -0300

    fix: redirect to crypto page as default

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ const nextConfig = {
     return [
       {
         source: '/',
-        destination: '/brl/staking',
+        destination: '/brl/crypto',
         permanent: false
       }
     ]
