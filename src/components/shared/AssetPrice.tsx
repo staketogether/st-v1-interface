bd137e4f3e53209f83d89dedfcda5cc34b37b891
Wellington Fernandes <wellingtonfds@gmail.com>
2024-05-10 14:19:14 -0300

    Feat/add percente change 24h (#260)
    * feat: add percentage
    
    * Update src/hooks/useGetCurrencyPrice.tsx
    
    Co-authored-by: Arthur <arthurmr13@hotmail.com>
    
    * feat: rename var
    
    ---------
    
    Co-authored-by: Arthur <arthurmr13@hotmail.com>
    Co-authored-by: Paulo Barcelos <46430156+paulobarcelosbr@users.noreply.github.com>

--- src/components/shared/AssetPrice.tsx
+++ src/components/shared/AssetPrice.tsx
@@ -8,6 +8,11 @@ interface AssetPriceProps {
 }
 
 export default function AssetPrice({ asset, className }: AssetPriceProps) {
-  const { priceConvertedValue, loading } = useCoinConversion('1', asset.chains[0], asset.contractAddress)
-  return loading ? <SkeletonLoading width={64} /> : <span className={className}>{priceConvertedValue}</span>
+
+  const { priceConvertedValue, loading, priceChangePercentage24h } = useCoinConversion('1', asset.chains[0], asset.contractAddress)
+  const signalPercentChange24h = priceChangePercentage24h && priceChangePercentage24h > 0 ? '+' : ''
+  return loading ? <SkeletonLoading width={64} /> : <>
+    <span className={className}>{priceConvertedValue}</span>
+    {priceChangePercentage24h && <span className={priceChangePercentage24h > 0 ? 'price-up' : 'price-down'}>{`${signalPercentChange24h}${priceChangePercentage24h.toFixed(2)}%`}</span >}
+  </>
 }
