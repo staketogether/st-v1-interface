e8b2331475fc364c9713c05e335fb14a0d2a86b1
wellington Fernandes <wellingtonfds@gmail.com>
2024-05-27 14:13:37 -0300

    feat: update layout actions

--- src/components/shared/NavActions.tsx
+++ src/components/shared/NavActions.tsx
@@ -76,6 +76,13 @@ const { Nav, IconContainer } = {
 
           &.disabled {
             cursor: not-allowed;
+            background: ${({ theme }) => theme.colorV2.foreground};
+            background: ${({ theme }) => theme.colorV2.white};
+            border: 1px solid ${({ theme }) => theme.colorV2.foreground};
+            &:hover {
+              filter: grayscale(100%);
+              opacity: 0.6;
+            }
           }
         }
 
