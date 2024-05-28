bd214208bf60c7c8865963e3d51b9b45973c0988
Arthur RIbeiro <arthurmr13@hotmail.com>
2024-05-28 17:40:21 -0300

    fix: removed pipedrive chatbot

--- src/components/shared/layout/LayoutTemplate.tsx
+++ src/components/shared/layout/LayoutTemplate.tsx
@@ -7,7 +7,6 @@ import styled from 'styled-components'
 import { lightTheme } from '../../../styles/theme'
 import { Cloudflare } from '../scripts/Cloudflare'
 import { GoogleTag } from '../scripts/GoogleTag'
-import { PipeDrive } from '../scripts/PipeDrive'
 import LayoutFooter from './LayoutFooter'
 import LayoutHeaderDesktop from './LayoutHeaderDesktop'
 import LayoutHeaderMobile from './LayoutHeaderMobile'
@@ -55,7 +54,6 @@ export default function LayoutTemplate({ children }: LayoutTemplateProps) {
         <>
           <GoogleTag />
           <Cloudflare />
-          <PipeDrive />
         </>
       )}
       <NextNProgress color={lightTheme.color.secondary} options={{ showSpinner: false }} />
