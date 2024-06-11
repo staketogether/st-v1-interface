f0c87df84057a53b16e82887f4188a6a77230267
Paulo Barcelos <paulo.barcelosti@gmail.com>
2024-06-11 10:42:38 -0300

    fix: fix

--- src/components/assets/AssetsRampControl.tsx
+++ src/components/assets/AssetsRampControl.tsx
@@ -33,8 +33,10 @@ export default function AssetsRampControl({ asset, type, userTokenBalance, userT
   const { address: walletAddress } = useAccount()
 
   const currentStep = useReactiveVar(rampStepControlVar)
-  useEffect(() => {}, [currentStep, type])
-
+  useEffect(() => {
+    if (!currentStep) rampStepControlVar(type === 'buy' ? RampSteps.Quotation : RampSteps.QuotationOffRamp)
+  }, [currentStep, type])
+  console.log('loop')
   const titleList: Record<string, string> = {
     Success: type === 'buy' ? t('v2.ramp.onRamp.success') : t('v2.ramp.offRamp.success'),
     MethodPayment: t('v2.ramp.provider')
