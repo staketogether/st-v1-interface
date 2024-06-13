23772499f498c94b977c4685014ed88d38fe33e5
Samuel <gmc103709@gmail.com>
2024-06-13 20:33:28 +0100

    feat: refatored code

--- src/components/shared/EditAccount.tsx
+++ src/components/shared/EditAccount.tsx
@@ -1,81 +1,92 @@
+import useConnectedAccount from '@/hooks/useConnectedAccount'
+import useLocaleTranslation from '@/hooks/useLocaleTranslation'
 import { FiTrash2 } from 'react-icons/fi'
 import styled from 'styled-components'
-
+import { useAccount } from 'wagmi'
 
 export default function EditAccount() {
+
+  const { t } = useLocaleTranslation()
 
   return (
     <>
       <Container>
-        <FormSection>
-          <h3>Profile</h3>
-          <div>
-            <label htmlFor="">User Name</label>
-            <Input type="text" placeholder="Samuel" />
-          </div>
-          <div>
-            <label htmlFor="">E-mail</label>
-            <Input type="text" placeholder="email@exemple.com" />
-          </div>
-          <Button>Edit</Button>
-        </FormSection>
-        <FormSection>
-          <h3>KYC</h3>
+        <Section>
+          <Header>
+            <h3>{t('web3AuthWalletSettings.profile')}</h3>
+            <Button disabled>{t('soon')}</Button>
+          </Header>
+          <WrapperInfo>
+            <span>{t('web3AuthWalletSettings.userName')}</span>
+            <Span>Nome</Span>
+          </WrapperInfo>
+          <WrapperInfo>
+            <span>{t('web3AuthWalletSettings.email')}</span>
+            <Span>email@exemple.com</Span>
+          </WrapperInfo>
+        </Section>
+        <Section>
+          <Header>
+            <h3>KYC</h3>
+            <Button disabled>{t('soon')}</Button>
+          </Header>
           <KYCard>
-            <div>
-              <label htmlFor="">KYC Level:</label>
-              <Input type="text" placeholder="0" />
-            </div>
-            <div>
-              <label htmlFor="">Limite de compra:</label>
-              <Input type="text" placeholder="R$ 0" />
-            </div>
-            <div>
-              <label htmlFor="">Limite usado:</label>
-              <Input type="text" placeholder="R$ 0" />
-            </div>
+            <WrapperInfo>
+              <span>{t('web3AuthWalletSettings.kycLevel')}:</span>
+              <span>0</span>
+            </WrapperInfo>
+            <WrapperInfo>
+              <span>{t('web3AuthWalletSettings.purchaseLimit')}:</span>
+              <span>R$ 0</span>
+            </WrapperInfo>
+            <WrapperInfo>
+              <span>{t('web3AuthWalletSettings.limitUsed')}:</span>
+              <span>R$ 0</span>
+            </WrapperInfo>
           </KYCard>
-          <Button>Atualizar KYC</Button>
-        </FormSection>
-        <FormSection>
-          <h3>Chave PIX</h3>
+        </Section>
+        <Section>
+          <Header>
+            <h3>{t('web3AuthWalletSettings.pixKey')}</h3>
+            <Button disabled>{t('soon')}</Button>
+          </Header>
           <Wrapper>
-            <div>
-              <label htmlFor="">Chave PIX</label>
-              <Input type="text" placeholder="fulano@gmail.com" />
-           </div>
-            <FiTrash2 size={24}/>
+            <WrapperInfo>
+              <span >{t('web3AuthWalletSettings.pixKey')}</span>
+              <span>email@exemple.com</span>
+            </WrapperInfo>
+            <FiTrash2 size={24} />
           </Wrapper>
-          <Button>Cadastrar nova chave</Button>
-        </FormSection>
-        <FormSection>
-          <h3>Wallets</h3>
+        </Section>
+        <Section>
+          <Header>
+            <h3>{t('web3AuthWalletSettings.wallets')}</h3>
+            <Button disabled>{t('soon')}</Button>
+          </Header>
           <Wrapper>
-            <div>
-              <label htmlFor="">Address</label>
-              <Input type="text" placeholder="0x4f...d862A" />
-            </div>
-            <FiTrash2 size={24}/>
+            <WrapperInfo>
+              <span >{t('web3AuthWalletSettings.address')}</span>
+              <span>0x41f45...d862A</span>
+            </WrapperInfo>
+            <FiTrash2 size={24} />
           </Wrapper>
-          <Button>Cadastrar nova chave</Button>
-        </FormSection>
+        </Section>
       </Container>
     </>
   )
 }
 
 
-export const { Container, FormSection, KYCard, Wrapper, Input, Button } = {
+export const { Container, Section, Header, WrapperInfo, KYCard, Wrapper, Span, Button } = {
   Container: styled.div`
   display: flex;
   flex-direction: column;
-  gap: 20px;
-
+  gap: ${({ theme }) => theme.size[24]};
 `,
-  FormSection: styled.div`
+  Section: styled.div`
   display: flex;
   flex-direction: column;
-  gap: 10px;
+  gap: ${({ theme }) => theme.size[12]};
 
   h3 {
     font-size: ${({ theme }) => theme.font.size[15]};
@@ -84,87 +95,100 @@ export const { Container, FormSection, KYCard, Wrapper, Input, Button } = {
     line-height: normal;
     color: ${({ theme }) => theme.colorV2.gray[1]};
   }
-
-  div {
+`,
+  Header: styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  `,
+  WrapperInfo: styled.div`
     display: flex;
     flex-direction: column;
     gap: ${({ theme }) => theme.size[4]};
 
-    label {
+    span {
     color: ${({ theme }) => theme.colorV2.gray[1]};
     }
-  }
-`,
-  Input: styled.input`
+  `,
+  Span: styled.span`
   padding: 10px;
   border: 1px solid ${({ theme }) => theme.colorV2.gray[6]};
   border-radius: ${({ theme }) => theme.size[8]};
   outline: 0;
-  font-weight: 500;
-  font-size: ${({ theme }) => theme.font.size[15]};
-`,
+  font-weight: 400;
+  font-size: ${({ theme }) => theme.font.size[13]};
+  color: ${({ theme }) => theme.colorV2.gray[1]};
+  `,
   Button: styled.button`
-  padding: 10px;
-  background-color: #4a4aad;
-  color: ${({ theme }) => theme.color.white};
-  border: none;
-  border-radius: ${({ theme }) => theme.size[8]};
-  cursor: pointer;
+    display: flex;
+    height: 24px;
+    padding: 0px ${({ theme }) => theme.size[16]};
+    justify-content: center;
+    align-items: center;
+    border-radius: ${({ theme }) => theme.size[4]};
+    border: 1px solid ${({ theme }) => theme.color.primary};
+    background: transparent;
+    outline: 0;
+    color: ${({ theme }) => theme.color.primary};
+    font-weight: 500;
 
-  &:hover {
-    background-color: #3939a2;
-  }
+    &:disabled {
+      opacity: 0.4;
+    }
   `,
   KYCard: styled.div`
+  width: 100%;
   border: 1px solid ${({ theme }) => theme.colorV2.gray[6]};
   border-radius: ${({ theme }) => theme.size[4]};
   padding: ${({ theme }) => theme.size[8]};
-  flex-direction: row !important;
+  display: flex;
+  flex-direction: row;
   justify-content: space-between;
   border-radius: ${({ theme }) => theme.size[8]};
+  align-items: center;
+  align-self: stretch;
 
   div {
-    width: 100%;
-  }
-
-  div:nth-child(3) {
-    flex: 1;
-    align-items: flex-end;
-
-    label {
-      white-space: nowrap;
+    width: auto;
+    span {
+      font-size: ${({ theme }) => theme.font.size[12]};
+      font-weight: 400;
     }
-  }
- 
-  input {
-    border: 0;
-    padding: 0;
-    width: 100%;
-    color: ${({ theme }) => theme.colorV2.gray[1]};
-    font-weight: bold;
-    font-size: ${({ theme }) => theme.font.size[15]};
+    span:nth-child(2) {
+      font-size: ${({ theme }) => theme.font.size[15]};
+      font-style: normal;
+      font-weight: 500;
+      line-height: normal;
+      align-self: left;
+    }
+
+     @media (min-width: ${({ theme }) => theme.breakpoints.sm}) {
+      span {
+            font-size: ${({ theme }) => theme.font.size[13]};
+      }
+     }
   }
   `,
   Wrapper: styled.div`
   border: 1px solid ${({ theme }) => theme.colorV2.gray[6]};
   border-radius: ${({ theme }) => theme.size[4]};
   padding: ${({ theme }) => theme.size[8]};
+  display: flex;
   flex-direction: row !important;
   justify-content: space-between;
   align-items: center;
   border-radius: ${({ theme }) => theme.size[8]};
 
- div {
-  width :100%;
-   label {
-    color: ${({ theme }) => theme.color.gray[400]} !important;
+   span {
+    color: ${({ theme }) => theme.color.gray[400]};
+  }
+  span:nth-child(2) {
+    color: ${({ theme }) => theme.colorV2.gray[1]};
+    font-weight: 500;
   }
 
-  input {
-    border: 0;
-    padding: 0;
-    width: 100%;
-  }
+ svg {
+  cursor: pointer;
  }
   `
 }
--- src/components/wallet/WalletSidebarWeb3AuthSettings.tsx
+++ src/components/wallet/WalletSidebarWeb3AuthSettings.tsx
@@ -1,13 +1,10 @@
-import { web3AuthInstanceVar } from '@/config/web3Auth'
+import { chainConfigByChainId } from '@/config/chain'
 import useLocaleTranslation from '@/hooks/useLocaleTranslation'
-import { useReactiveVar } from '@apollo/client'
-import { useEffect, useState } from 'react'
-import { AiOutlineClose } from 'react-icons/ai'
-import { PiArrowRight } from 'react-icons/pi'
+import etherscan from '@assets/icons/etherscan.svg'
+import Image from 'next/image'
 import styled from 'styled-components'
-import DefaultButton from '../shared/Button'
-import Modal from '../shared/Modal'
 import EditAccount from '../shared/EditAccount'
+import { PiArrowLeft } from 'react-icons/pi'
 
 interface web3AuthWalletSettingsProps {
   setWeb3authWalletActive?: (value: boolean) => void
@@ -16,56 +13,41 @@ interface web3AuthWalletSettingsProps {
 
 export default function WalletSidebarWeb3AuthWalletSettings({
   setWeb3authWalletActive: setIsWalletSidebarWeb3AuthSettingsActive,
+  walletAddress
 }: web3AuthWalletSettingsProps) {
-  const [notifyModal, setNotifyModal] = useState(false)
 
   const { t } = useLocaleTranslation()
-
-  const web3AuthInstance = useReactiveVar(web3AuthInstanceVar)
-
-  useEffect(() => {
-    const getPrivateKey = async () => {
-    }
-    getPrivateKey()
-  }, [web3AuthInstance])
+  const optimism = chainConfigByChainId(10)
+  const ethereum = chainConfigByChainId(1)
 
   return (
     <>
       <Header>
-        <h2>Edit Account</h2>
+        <h2>{t('web3AuthWalletSettings.editAccount')}</h2>
         <Button onClick={() => setIsWalletSidebarWeb3AuthSettingsActive && setIsWalletSidebarWeb3AuthSettingsActive(false)}>
-          <AiOutlineClose />
+          <PiArrowLeft />
         </Button>
       </Header>
       <EditAccount />
-      <Modal title={t('web3AuthWalletSettings.modal.title')} isOpen={notifyModal} onClose={() => setNotifyModal(false)} width={420}>
-        <AlertMessageContainer>
-          <div>
-            <span className='bold'>{t('web3AuthWalletSettings.modal.description')}</span>
-            <span>{t('web3AuthWalletSettings.modal.role1')}</span>
-            <span>{t('web3AuthWalletSettings.modal.role2')}</span>
-            <span>{t('web3AuthWalletSettings.modal.role3')}</span>
-            <span>{t('web3AuthWalletSettings.modal.role4')}</span>
-          </div>
-          <footer>
-            <DefaultButton
-              onClick={() => {
-                setNotifyModal(false)
-              }}
-              label={t('next')}
-              block
-              ghost
-              icon={<PiArrowRight />}
-            />
-            <DefaultButton onClick={() => setNotifyModal(false)} label={t('cancel')} block icon={<AiOutlineClose />} />
-          </footer>
-        </AlertMessageContainer>
-      </Modal>
+      <Container>
+        <a className='copy' href={`${ethereum.blockExplorer.baseUrl}/address/${walletAddress}`} target='_blank'>
+          <Card>
+            <Image src={etherscan} alt='etherscan icon' width={16} height={16} />
+            {t('web3AuthWalletSettings.showEtherScan')}
+          </Card>
+        </a>
+        <a className='copy' href={`${optimism.blockExplorer.baseUrl}/address/${walletAddress}`} target='_blank'>
+          <Card>
+            <Image src={etherscan} alt='etherscan icon' width={16} height={16} />
+            {t('web3AuthWalletSettings.showOptimismScan')}
+          </Card>
+        </a>
+      </Container>
     </>
   )
 }
 
-const { Header, Button, AlertMessageContainer } = {
+const { Header, Button, Container, Card } = {
   Header: styled.div`
     min-height: 32px;
     width: 100%;
@@ -85,22 +67,48 @@ const { Header, Button, AlertMessageContainer } = {
       font-weight: 500;
     }
   `,
+  Container: styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: ${({ theme }) => theme.size[24]};
+    padding-top: ${({ theme }) => theme.size[12]};
+  `,
+  Card: styled.div`
+    width: 100%;
+    padding: 0px 16px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: ${({ theme }) => theme.size[4]};
+
+    height: 42px;
+    border-radius: ${({ theme }) => theme.size[8]};
+    background: ${({ theme }) => theme.color.white};
+    box-shadow: ${({ theme }) => theme.shadow[100]};
+    border: 1px solid ${({ theme }) => theme.color.primary};
+    font-size: ${({ theme }) => theme.font.size[15]};
+    color: ${({ theme }) => theme.colorV2.gray[1]};
+    font-weight: 400;
+
+    cursor: pointer;
+    &:hover {
+      background: ${({ theme }) => theme.color.whiteAlpha[600]};
+    }
+  `,
   Button: styled.button`
-    display: grid;
+     display: grid;
     align-items: center;
     justify-content: center;
     width: 32px;
     height: 32px;
     border: 0;
+    border-radius: ${({ theme }) => theme.size[8]};
+    box-shadow: ${({ theme }) => theme.shadow[100]};
     background: ${({ theme }) => theme.color.white};
     transition: background 0.2s ease;
     line-height: 36px;
-    display: flex;
-    gap: 10px;
-    border-radius: ${({ theme }) => theme.size[8]};
-    background: ${({ theme }) => theme.color.white};
-    box-shadow: 0px 2px 1px 0px rgba(0, 0, 0, 0.20);
-    
+
     &:hover {
       background: ${({ theme }) => theme.color.whiteAlpha[600]};
     }
@@ -110,36 +118,10 @@ const { Header, Button, AlertMessageContainer } = {
     }
 
     svg {
-      width: 9.894px;
-      height: 10.188px;
+      width: 24px;
+      height: 24px;
       flex-shrink: 0;
       color: ${({ theme }) => theme.colorV2.blue[3]};
     }
   `,
-  AlertMessageContainer: styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: ${({ theme }) => theme.size[24]};
-
-    div {
-      display: flex;
-      flex-direction: column;
-      gap: ${({ theme }) => theme.size[8]};
-
-      span {
-        color: ${({ theme }) => theme.colorV2.gray[1]};
-        font-size: 13px;
-        font-weight: 400;
-        &.bold {
-          font-weight: 500;
-        }
-      }
-    }
-
-    > footer {
-      display: flex;
-      flex-direction: column;
-      gap: ${({ theme }) => theme.size[4]};
-    }
-  `
 }
