59d521335b973d2e21a59614af55d84af930c113
Samuel <gmc103709@gmail.com>
2024-06-14 16:25:55 +0100

    feat: create get hook data

---
+++ src/hooks/userKyc.tsx
@@ -0,0 +1,16 @@
+import useSWR from 'swr';
+import axios from 'axios';
+import { globalConfig } from '@/config/global'
+
+
+export default function userKyc(id: string) {
+  const { backendUrl } = globalConfig
+  const fetcher = (uri: string) => axios.get(`${backendUrl}/${uri}`).then((res) => res.data);
+  const { data, error } = useSWR(id ? `/api/user-kyc/${id}` : null, fetcher);
+
+  return {
+    userKyc: data,
+    isLoading: !error && !data,
+    isError: error
+  };
+}
