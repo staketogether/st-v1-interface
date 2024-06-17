316f8ca32a6ba04b1c0e691858599cf12dafd493
Samuel <gmc103709@gmail.com>
2024-06-17 18:28:00 +0100

    fix: fix asset price logic (#322)

--- src/components/shared/AssetPrice.tsx
+++ src/components/shared/AssetPrice.tsx
@@ -11,7 +11,6 @@ interface AssetPriceProps {
 
 export default function AssetPrice({ asset, className, showChangePercentage = false }: AssetPriceProps) {
   const { priceConvertedValue, loading, priceChangePercentage24h } = useCoinConversion('1', asset.chains[0], asset.contractAddress)
-
   const signalPercentChange24h = priceChangePercentage24h && priceChangePercentage24h > 0 ? '+' : ''
   return loading ? (
     <ContainerLoading>
@@ -21,7 +20,7 @@ export default function AssetPrice({ asset, className, showChangePercentage = fa
   ) : (
     <>
       <span className={className}>{priceConvertedValue}</span>
-      {priceChangePercentage24h && showChangePercentage && (
+        {!!(priceChangePercentage24h && showChangePercentage) && (
         <span
           className={priceChangePercentage24h > 0 ? 'price-up' : 'price-down'}
         >{`${signalPercentChange24h}${priceChangePercentage24h.toFixed(2)}%`}</span>
