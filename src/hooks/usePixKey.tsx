039e27f53bf54e2351ec74260a0c3b95b57777d3
Samuel <gmc103709@gmail.com>
2024-06-18 18:43:08 +0100

    fix: fix

--- src/hooks/usePixKey.tsx
+++ src/hooks/usePixKey.tsx
@@ -1,20 +1,22 @@
 import useSWR from 'swr';
 import axios from 'axios';
-import { globalConfig } from '@/config/global'
+import { globalConfig } from '@/config/global';
 
 interface UserPixKey {
-  pixKey: string,
-  type: string
+  pixKey: string;
+  type: string;
 }
 
-export default function userPixKey(id: number) {
-  const { backendUrl } = globalConfig
-  const fetcher = (uri: string) => axios.get(`${backendUrl}/${uri}`).then((res) => res.data);
-  const { data, error } = useSWR<Array<UserPixKey>>(id ? `/api/pixkeys/${id}` : null, fetcher);
+export default function userPixKey(id: number | null) {
+  const { backendUrl } = globalConfig;
+  const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+
+  const url = id ? `${backendUrl}/api/pixkeys/${id}` : null;
+  const { data, error } = useSWR<UserPixKey[]>(url, fetcher);
 
   return {
     pixKey: data,
     isLoading: !error && !data,
-    isError: error
+    isError: error,
   };
 }
