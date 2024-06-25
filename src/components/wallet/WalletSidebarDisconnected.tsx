6365168950229a3f522376623fdcfbefdfd26301
Samuel <gmc103709@gmail.com>
2024-06-25 18:56:49 +0100

    update WalletSidebarDisconnected.tsx

--- src/components/wallet/WalletSidebarDisconnected.tsx
+++ src/components/wallet/WalletSidebarDisconnected.tsx
@@ -39,6 +39,7 @@ export default function WalletSidebarDisconnected() {
             </Actions>
           </HeaderContainer>
           <ConnectWallet />
+        </>
       )}
     </DrawerContainer>
   )
